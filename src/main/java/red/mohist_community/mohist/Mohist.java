282a68fa397989732a95362722c9b5b5206e5fb3
a1640727878 <a1640727878@163.com>
2019-08-25 17:39:42 +0800

    Test Mixin

---
+++ src/main/java/red/mohist_community/mohist/Mohist.java
@@ -0,0 +1,8 @@
+package red.mohist_community.mohist;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "mohist", name = "Mohist")
+public class Mohist {
+
+}
---
+++ src/main/java/red/mohist_community/mohist/MohistCoreMod.java
@@ -0,0 +1,45 @@
+package red.mohist_community.mohist;
+
+import java.util.Map;
+
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.SortingIndex(-5000)
+@IFMLLoadingPlugin.Name("Mohist")
+public class MohistCoreMod implements IFMLLoadingPlugin {
+
+	public MohistCoreMod() {
+		MixinBootstrap.init();
+		Mixins.addConfiguration("mixins.mohist.json");
+	}
+	
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+		
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+
+}
