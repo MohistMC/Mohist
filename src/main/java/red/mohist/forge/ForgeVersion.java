0087ec776f2eb034361989707bd73221689c17b1
Mgazul <mgazul@163.com>
2019-09-14 14:31:07 +0800

    Now you are free to set the forge version number

--- src/main/java/red/mohist/forge/ForgeVersion.java
+++ src/main/java/red/mohist/forge/ForgeVersion.java
@@ -1,9 +1,11 @@
 package red.mohist.forge;
 
+import red.mohist.configuration.MohistConfig;
+
 public class ForgeVersion {
 
-    public static final int major = 14;
-    public static final int minor = 23;
-    public static final int revision = 5;
-    public static final int build = 2847;
+    public static final int major = MohistConfig.instance.major.getValue();
+    public static final int minor = MohistConfig.instance.minor.getValue();
+    public static final int revision = MohistConfig.instance.revision.getValue();
+    public static final int build = MohistConfig.instance.build.getValue();
 }
