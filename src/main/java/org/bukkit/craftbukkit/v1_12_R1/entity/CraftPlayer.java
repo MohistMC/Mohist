4739e4b4177c85ecf567adfb880616f37e23b7fc
Mgazul <mgazul@163.com>
2019-09-26 17:11:01 +0800

    Fixed #12

--- src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_12_R1/entity/CraftPlayer.java
@@ -1382,7 +1382,6 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     }
 
     public void addChannel(String channel) {
-        com.google.common.base.Preconditions.checkState( channels.size() < 128, "Too many channels registered" ); // Spigot
         if (channels.add(channel)) {
             server.getPluginManager().callEvent(new PlayerRegisterChannelEvent(this, channel));
         }
