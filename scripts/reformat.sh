157113ae707ba92c64f3058b8acd57dbe4fad801
Mgazul <mgazul@163.com>
2019-11-21 18:45:59 +0800

    del res

--- scripts/reformat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-find patches -type f -name '*.java.patch' -exec sed -i "s/\t/    /g" {} \;
-find src/main/java -type f -name '*.java' -exec sed -i "s/\t/    /g" {} \;
-
--- scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git submodule update --init --recursive
-./gradlew setupCauldron installBundle
