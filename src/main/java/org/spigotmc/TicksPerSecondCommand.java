e99f425b22aec0d74d7578a1365e9b683523e718
Mgazul <mgazul@163.com>
2019-11-24 01:32:57 +0800

    Update TicksPerSecondCommand.java

--- src/main/java/org/spigotmc/TicksPerSecondCommand.java
+++ src/main/java/org/spigotmc/TicksPerSecondCommand.java
@@ -1,8 +1,12 @@
 package org.spigotmc;
 
+import net.minecraft.server.MinecraftServer;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import red.mohist.util.i18n.Message;
 
 public class TicksPerSecondCommand extends Command {
@@ -25,7 +29,51 @@ public class TicksPerSecondCommand extends Command {
             sender.sendMessage(Message.getString("command.nopermission"));
             return true;
         }
+        World currentWorld = null;
+        if (sender instanceof CraftPlayer) {
+            currentWorld = ((CraftPlayer) sender).getWorld();
+        }
+        sender.sendMessage(ChatColor.DARK_RED + "---------------------------------------");
+        final MinecraftServer server = MinecraftServer.getServerInst();
+        ChatColor colourTPS;
+        for (World world : server.server.getWorlds()) {
+            if (world instanceof CraftWorld) {
+                boolean current = currentWorld != null && currentWorld == world;
+                net.minecraft.world.WorldServer mcWorld = ((CraftWorld) world).getHandle();
+                String bukkitName = world.getName();
+                int dimensionId = mcWorld.provider.getDimension();
+                String name = mcWorld.provider.getDimensionType().getName();
+                String displayName = name.equals(bukkitName) ? name : String.format("%s | %s", name, bukkitName);
 
+                double worldTickTime = mean(server.worldTickTimes.get(dimensionId)) * 1.0E-6D;
+                double worldTPS = Math.min(1000.0 / worldTickTime, 20);
+
+                if (worldTPS >= 18.0) {
+                    colourTPS = ChatColor.GREEN;
+                } else if (worldTPS >= 15.0) {
+                    colourTPS = ChatColor.YELLOW;
+                } else {
+                    colourTPS = ChatColor.RED;
+                }
+
+                sender.sendMessage(String.format("%s[%d] %s%s %s- %s%.2fms / %s%.2ftps", ChatColor.GOLD, dimensionId,
+                        current ? ChatColor.GREEN : ChatColor.YELLOW, displayName, ChatColor.RESET,
+                        ChatColor.DARK_RED, worldTickTime, colourTPS, worldTPS));
+            }
+        }
+
+        double meanTickTime = mean(server.tickTimeArray) * 1.0E-6D;
+        double meanTPS = Math.min(1000.0 / meanTickTime, 20);
+        if (meanTPS >= 18.0) {
+            colourTPS = ChatColor.GREEN;
+        } else if (meanTPS >= 15.0) {
+            colourTPS = ChatColor.YELLOW;
+        } else {
+            colourTPS = ChatColor.RED;
+        }
+        sender.sendMessage(String.format("%sOverall - %s%s%.2fms / %s%.2ftps", ChatColor.BLUE, ChatColor.RESET,
+                ChatColor.DARK_RED, meanTickTime, colourTPS, meanTPS));
+        sender.sendMessage(ChatColor.DARK_RED + "---------------------------------------");
         // Paper start - Further improve tick handling
         double[] tps = org.bukkit.Bukkit.getTPS();
         String[] tpsAvg = new String[tps.length];
@@ -36,4 +84,12 @@ public class TicksPerSecondCommand extends Command {
 
         return true;
     }
+
+    private static final long mean(long[] array) {
+        long r = 0;
+        for (long i : array)
+            r += i;
+        return r / array.length;
+    }
+
 }
