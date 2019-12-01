d1a31ff1e5b9402246e510d42e3965751ad78d5a
lvyitian <15988566900@qq.com>
2019-12-01 10:01:53 +0800

    Update MohistCommand.java

--- src/main/java/red/mohist/command/MohistCommand.java
+++ src/main/java/red/mohist/command/MohistCommand.java
@@ -75,7 +75,7 @@ public class MohistCommand extends Command {
                 sender.sendMessage(ChatColor.GREEN +ServerAPI.getLanguage());
                 break;
             case "item":
-                if ("info".equals(args[1].toLowerCase(Locale.ENGLISH))){
+                if (args.Length > 0 && "info".equals(args[1].toLowerCase(Locale.ENGLISH))){
                     ItemCommand.info(sender);
                 }
             default:
