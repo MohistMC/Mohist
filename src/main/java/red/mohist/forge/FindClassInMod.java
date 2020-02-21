c5d1f5e346257cb617a354e687adc6fc8d50bfe1
Mgazul <mgazul@163.com>
2020-02-22 00:29:28 +0800

    Filter sponge

--- src/main/java/red/mohist/forge/FindClassInMod.java
+++ src/main/java/red/mohist/forge/FindClassInMod.java
@@ -94,7 +94,7 @@ public class FindClassInMod {
     public static void jar() throws Exception {
         String libDir = "mods";
         // example: red.mohist.Mohsit
-        List<String> classlist = Arrays.asList("com.tmtravlr.jaff.entities.EntityFish" /*JustAFewFish*/, "cccc.cccc.cccc");
+        List<String> classlist = Arrays.asList("com.tmtravlr.jaff.entities.EntityFish" /*JustAFewFish*/, "org.spongepowered.mod.SpongeMod");
         for (String classname : classlist) {
             classname = classname.replaceAll("\\.", "/") + ".class";
 
