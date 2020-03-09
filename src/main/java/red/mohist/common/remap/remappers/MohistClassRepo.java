09b8d6acc1e8e40ccf876f561ea59858710fbd9d
Mgazul <mgazul@163.com>
2020-03-09 15:07:13 +0800

    Update MohistClassRepo.java

--- src/main/java/red/mohist/common/remap/remappers/MohistClassRepo.java
+++ src/main/java/red/mohist/common/remap/remappers/MohistClassRepo.java
@@ -27,7 +27,7 @@ public class MohistClassRepo extends CachingRepo {
         }
         ClassNode classNode = new ClassNode();
         try {
-            ClassReader reader = new ClassReader(readClass(in, false));
+            ClassReader reader = new ClassReader(in);
             reader.accept(classNode, 0);
         } catch (IOException e) {
             return null;
@@ -44,42 +44,5 @@ public class MohistClassRepo extends CachingRepo {
             cl = this.getClass().getClassLoader();
         }
         return cl;
-    }
-
-    private static byte[] readClass(final InputStream is, boolean close)
-            throws IOException {
-        if (is == null) {
-            throw new IOException("Class not found");
-        }
-        try {
-            byte[] b = new byte[is.available()];
-            int len = 0;
-            while (true) {
-                int n = is.read(b, len, b.length - len);
-                if (n == -1) {
-                    if (len < b.length) {
-                        byte[] c = new byte[len];
-                        System.arraycopy(b, 0, c, 0, len);
-                        b = c;
-                    }
-                    return b;
-                }
-                len += n;
-                if (len == b.length) {
-                    int last = is.read();
-                    if (last < 0) {
-                        return b;
-                    }
-                    byte[] c = new byte[b.length + 1000];
-                    System.arraycopy(b, 0, c, 0, len);
-                    c[len++] = (byte) last;
-                    b = c;
-                }
-            }
-        } finally {
-            if (close) {
-                is.close();
-            }
-        }
     }
 }
