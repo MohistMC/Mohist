5b3d32cb4bca1d35fc60d5dea8c3c3b395af30a9
Mgazul <mgazul@163.com>
2020-03-12 00:32:19 +0800

    Allow motd packets to get mohist version

--- src/main/java/red/mohist/forge/MohistMod.java
+++ src/main/java/red/mohist/forge/MohistMod.java
@@ -1,9 +1,12 @@
 package red.mohist.forge;
 
 import com.google.common.eventbus.EventBus;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import net.minecraftforge.fml.common.DummyModContainer;
 import net.minecraftforge.fml.common.LoadController;
 import net.minecraftforge.fml.common.ModMetadata;
+import red.mohist.Mohist;
 
 public class MohistMod extends DummyModContainer {
 
@@ -17,8 +20,29 @@ public class MohistMod extends DummyModContainer {
     }
 
     @Override
-    public Disableable canBeDisabled()
-    {
+    public Disableable canBeDisabled() {
         return Disableable.YES;
+    }
+
+    public static InputStream modinfo() {
+        String info = "[\n" +
+                "{\n" +
+                "  \"modid\": \"mohist\",\n" +
+                "  \"name\": \"Mohist\",\n" +
+                "  \"description\": \"Mohist built-in mark.\",\n" +
+                "  \"version\": \" " + Mohist.getVersion() + "\",\n" +
+                "  \"mcversion\": \"1.12.2\",\n" +
+                "  \"logoFile\": \"/mohist_logo.png\",\n" +
+                "  \"url\": \"http://mohist.red/\",\n" +
+                "  \"updateUrl\": \"\",\n" +
+                "  \"authors\": [\"Mgazul\", \"CraftDream\", \"azbh111\", \"lliioollcn\", \"terrainwax\", \"lvyitian\", \"ChenHauShen\", \"Others\"],\n" +
+                "  \"credits\": \"Made by the Mohist-Community\",\n" +
+                "  \"parent\": \"\",\n" +
+                "  \"screenshots\": [],\n" +
+                "  \"dependencies\": []\n" +
+                "}\n" +
+                "]";
+        InputStream inputStrem = new ByteArrayInputStream(info.getBytes());
+        return inputStrem;
     }
 }
