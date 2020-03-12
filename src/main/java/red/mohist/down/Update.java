e8dc2c7a8351d4d58b1554c5ea541e4a1a8bfeaa
Mgazul <mgazul@163.com>
2020-03-12 13:07:17 +0800

    Update Update.java

--- src/main/java/red/mohist/down/Update.java
+++ src/main/java/red/mohist/down/Update.java
@@ -14,7 +14,7 @@ public class Update {
 
     // Why use a hard core to split a String? Because I didn't actually start Mohist before this, there is no lib load, we can't use lib.
     public static void hasLatestVersion() {
-        String str = "https://ci.codemc.io/job/Mohist-Community/job/Mohist-1.12.2/lastBuild/";
+        String str = "https://ci.codemc.io/job/Mohist-Community/job/Mohist-1.12.2/lastBuild/api/json";
         String ver = "https://raw.githubusercontent.com/Mohist-Community/Mohist/1.12.2/mohist.ver";
         String dl = "https://ci.codemc.io/job/Mohist-Community/job/Mohist-1.12.2/";
         try {
@@ -22,28 +22,32 @@ public class Update {
             System.out.println(Message.getString("update.stopcheck"));
             URL url = new URL(str);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(8000);
             connection.setRequestProperty("User-Agent", "Mohist");
             if (connection.getResponseCode() == 200) {
                 InputStream is = connection.getInputStream();
                 String commits = IOUtil.readContent(is);
-                String sha = "/build/distributions/Mohist-";
-                String date = "(Wed Mar ";
+                String sha = "\"SHA1\"";
 
                 String s0 = commits.substring(commits.indexOf(sha));
-                String s1 = s0.substring(s0.indexOf(sha) + sha.length());
-                String s2 = s1.substring(0, 7);
 
-                String oldver = Update.class.getPackage().getImplementationVersion();
-                String time = commits.substring(commits.indexOf(date));
-                String time1 = time.substring(time.indexOf(date) + date.length());
-                String time2 = time1.substring(0, 20);
+                String[] ss = s0.split("\n");
+                String b = ss[0].trim().replace(" ", "").replace("\"", "");
+                String ci_sha = b.substring(5, 12);
+
+                String jar_sha = Update.class.getPackage().getImplementationVersion();
 
                 String newversion = MohistConfigUtil.getUrlString(ver, Mohist.VERSION);
                 String oldversion = Mohist.VERSION;
-                if (oldver.equals(s2)) {
-                    System.out.println(Message.getFormatString("update.latest", new Object[]{oldversion, s2, oldver}));
+                if (jar_sha.equals(ci_sha)) {
+                    System.out.println(Message.getFormatString("update.latest", new Object[]{oldversion, ci_sha, jar_sha}));
                 } else {
-                    System.out.println(Message.getFormatString("update.old", new Object[]{oldversion, newversion, s2, time2, oldver, dl}));
+                    String time = "\"date\"";
+                    String time1 = commits.substring(commits.indexOf(time));
+                    String[] ss1 = time1.split("\n");
+                    String b1 = ss1[0].trim().replace("\"", "");
+                    String time3 = b1.substring(5, 24);
+                    System.out.println(Message.getFormatString("update.old", new Object[]{oldversion, newversion, ci_sha, time3, jar_sha, dl}));
                 }
                 is.close();
             } else {
