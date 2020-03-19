76bbf031b488794d4f710f8a56724dee322d9d09
kaziu687 <kaziu687@gmail.com>
2020-03-19 13:11:13 +0100

    TileEntity tick-interval feature

---
+++ src/main/java/red/mohist/configuration/TileEntityWorldConfig.java
@@ -0,0 +1,8 @@
+package red.mohist.configuration;
+
+public class TileEntityWorldConfig extends WorldConfig {
+    public TileEntityWorldConfig(String worldName, ConfigBase configFile)
+    {
+        super(worldName, configFile);
+    }
+}
