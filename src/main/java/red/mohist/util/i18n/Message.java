b588323572446ade9e56a1fce1ece3113fae20f7
lliioollcn <lliiooll@126.com>
2020-04-02 20:58:57 +0800

    add map cache to cut back i/o

--- src/main/java/red/mohist/util/i18n/Message.java
+++ src/main/java/red/mohist/util/i18n/Message.java
@@ -2,16 +2,29 @@ package red.mohist.util.i18n;
 
 import java.io.File;
 import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import red.mohist.configuration.MohistConfigUtil;
 
 public class Message {
 
-    public static ResourceBundle rb = ResourceBundle.getBundle("assets.mohist.lang.message", new Locale(getLanguage(), getCountry()), new UTF8Control());
+    private static ResourceBundle rb = ResourceBundle.getBundle("assets.mohist.lang.message", new Locale(getLanguage(), getCountry()), new UTF8Control());
+    private static Map<String, String> msgs = Maps.newHashMap();
 
     public static String getString(String key) {
-        return rb.getString(key);
+        // add map cache to cut back i/o
+        if (!msgs.containsKey(key)) {// if the map not have the value
+            String msg = rb.getString(key);// get string value
+            if (!Strings.isNullOrEmpty(msg)) {// the value must be not a null or empty
+                msgs.put(key, msg);// add value to map
+            }
+        }
+        return msgs.get(key);
     }
 
     public static String getFormatString(String key, Object[] f) {
