5c6abeb4d73eb1bc411d5f02d188e75c6e76635f
Shawiiz_z <55449195+Shawiizz@users.noreply.github.com>
2020-04-03 18:51:22 +0200

    Added something to enable us to put plugins in a json file

--- src/main/java/red/mohist/bukkit/AutoDeletePlugins.java
+++ src/main/java/red/mohist/bukkit/AutoDeletePlugins.java
@@ -1,24 +1,34 @@
 package red.mohist.bukkit;
 
-import java.util.Arrays;
-import java.util.List;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import red.mohist.util.FindClassInJar;
 
-/**
- * @author Mgazul
- * @date 2020/3/22 23:15
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AutoDeletePlugins {
-
-    public static final List<String> classlist;
-
-    static {
-        classlist = Arrays.asList("test.test");
-    }
 
     public static void jar() throws Exception {
         String libDir = "plugins";
-        for (String classname : AutoDeletePlugins.classlist) {
+        JsonElement root = null;
+        URLConnection request;
+        try {
+            request = new URL("https://shawiizz.github.io/plugins.json").openConnection();
+            request.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+            request.connect();
+            root = new JsonParser().parse(new InputStreamReader((InputStream) request.getContent()));
+        } catch (JsonIOException | JsonSyntaxException | IOException | NullPointerException ignored) {
+        }
+
+        for (String classname : new ArrayList<>(Arrays.asList(root.getAsJsonObject().get("list").toString().split(",")))) {
             classname = classname.replaceAll("\\.", "/") + ".class";
 
             FindClassInJar ins = new FindClassInJar(libDir, classname);
