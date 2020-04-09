7cb3cdd500c11a864bbf3cefb4d64acb36ea8cd1
Shawiizz <shaweereso@gmail.com>
2020-04-09 17:58:10 +0200

    Fail #1

--- src/main/java/red/mohist/forge/MohistForgeUtils.java
+++ src/main/java/red/mohist/forge/MohistForgeUtils.java
@@ -9,7 +9,7 @@ public class MohistForgeUtils {
 
     public static boolean modsblacklist(String modslist) {
         String[] mod = MohistConfig.instance.modsblacklist.getValue().split(";");
-        if(MohistConfigUtil.getBoolean(new File("mohist-config", "mohist.yml"), "disable_mods_whitelist:")) {
+        if(!MohistConfigUtil.getBoolean(new File("mohist-config", "mohist.yml"), "disable_mods_whitelist:")) {
             for (Object str : mod) {
                 if(str.toString().length() > 0 && !modslist.contains(str.toString())) return true;
             }
