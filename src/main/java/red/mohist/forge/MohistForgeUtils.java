c5d1caada302b3c73fa0b9fb7119458fab2afbaf
Mgazul <mgazul@163.com>
2020-04-11 00:30:39 +0800

    Update MohistForgeUtils.java

--- src/main/java/red/mohist/forge/MohistForgeUtils.java
+++ src/main/java/red/mohist/forge/MohistForgeUtils.java
@@ -1,5 +1,6 @@
 package red.mohist.forge;
 
+import java.util.Collection;
 import red.mohist.configuration.MohistConfig;
 import red.mohist.configuration.MohistConfigUtil;
 
@@ -10,8 +11,9 @@ public class MohistForgeUtils {
     public static boolean modsblacklist(String modslist) {
         String[] mod = MohistConfig.instance.modsblacklist.getValue().split(";");
         if(!MohistConfigUtil.getBoolean(new File("mohist-config", "mohist.yml"), "disable_mods_whitelist:")) {
-            for (Object str : mod) {
-                if(str.toString().length() > 0 && !modslist.contains(str.toString())) return true;
+            Collection list = java.util.Arrays.asList(mod);
+            for (Object str : list) {
+                if(str.toString().length() > 0 && modslist.contains(str.toString())) return true;
             }
         }
         return false;
