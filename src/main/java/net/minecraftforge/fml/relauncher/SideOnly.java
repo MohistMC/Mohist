e105d21efce2493fe01a3d7afbd8a2a8c6ac8679
Mgazul <mgazul@163.com>
2020-04-13 12:27:23 +0800

    Synchronized forge update (0683971）

--- src/main/java/net/minecraftforge/fml/relauncher/SideOnly.java
+++ src/main/java/net/minecraftforge/fml/relauncher/SideOnly.java
@@ -1,6 +1,6 @@
 /*
  * Minecraft Forge
- * Copyright (c) 2016-2018.
+ * Copyright (c) 2016-2020.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,7 +19,7 @@
 
 /*
  * Minecraft Forge
- * Copyright (c) 2016-2018.
+ * Copyright (c) 2016-2020.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
