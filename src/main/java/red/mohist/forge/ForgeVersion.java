c81c34dc233fddcf44dc2e59e86efc1c71bc2672
Mgazul <mgazul@163.com>
2020-04-21 11:30:29 +0800

    Fixed #244

--- src/main/java/red/mohist/forge/ForgeVersion.java
+++ src/main/java/red/mohist/forge/ForgeVersion.java
@@ -1,8 +1,16 @@
 package red.mohist.forge;
 
+import java.io.File;
 import java.util.Map;
+import red.mohist.configuration.MohistConfigUtil;
 
 public class ForgeVersion {
+
+    private static final File f = new File("mohist-config", "mohist.yml");
+    public static final int major = MohistConfigUtil.getInt(f, "major:", "14");
+    public static final int minor = MohistConfigUtil.getInt(f, "minor:", "23");
+    public static final int revision = MohistConfigUtil.getInt(f, "revision:", "5");
+    public static final int build = MohistConfigUtil.getInt(f, "build:", "2854");
 
     public static boolean isCompatibleLowForge(Map<String, String> modList) {
         String forgeVersion = modList.get("forge");
