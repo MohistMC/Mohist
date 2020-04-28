1498b80f2c0599bdd80e7f9b77a9288bb2c874a2
Mgazul <mgazul@163.com>
2020-04-28 17:12:19 +0800

    Update forge to 1.15.2-31.1.62(ea23cd4)

--- src/fmllauncher/java/net/minecraftforge/fml/loading/FMLServerLaunchProvider.java
+++ src/fmllauncher/java/net/minecraftforge/fml/loading/FMLServerLaunchProvider.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import joptsimple.OptionSet;
 
 public class FMLServerLaunchProvider extends FMLCommonLaunchHandler implements ILaunchHandlerService
 {
