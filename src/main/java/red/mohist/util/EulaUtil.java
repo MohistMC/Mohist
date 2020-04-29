4163ac9845ede579337616994d57acb0d10a140c
Mgazul <mgazul@163.com>
2020-04-29 15:06:25 +0800

    Fixed #267

--- src/main/java/red/mohist/util/EulaUtil.java
+++ src/main/java/red/mohist/util/EulaUtil.java
@@ -5,10 +5,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.apache.commons.io.FileUtils;
 import red.mohist.util.i18n.Message;
 
 public class EulaUtil {
@@ -21,7 +19,7 @@ public class EulaUtil {
 
     public static boolean hasAcceptedEULA() throws IOException {
         if(new File("eula.txt").exists())
-            return FileUtils.readFileToString(new File("eula.txt"), StandardCharsets.UTF_8).contains("eula=true");
+            return FileUtil.readContent(new File("eula.txt")).contains("eula=true");
         return false;
     }
 }
