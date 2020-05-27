17615caa81209da9ce0dbd663aa1745622445855
Mgazul <mgazul@163.com>
2020-05-27 11:14:03 +0800

    Fixed #299 Add source block to BlockPhysicsEvent

--- src/main/java/org/bukkit/event/block/BlockPhysicsEvent.java
+++ src/main/java/org/bukkit/event/block/BlockPhysicsEvent.java
@@ -12,7 +12,39 @@ public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final int changed;
     private boolean cancel = false;
+    // Paper start - add source block
+    private int sourceX;
+    private int sourceY;
+    private int sourceZ;
+    private Block sourceBlock;
 
+    /**
+     *
+     * @deprecated Magic value
+     * @param block the block involved in this event
+     * @param changed the changed block's type id
+     * @param sourceX the x of the source block
+     * @param sourceY the y of the source block
+     * @param sourceZ the z of the source block
+     */
+    @Deprecated
+    public BlockPhysicsEvent(final Block block, final int changed, final int sourceX, final int sourceY, final int sourceZ) {
+        this(block, changed);
+        this.sourceX = sourceX;
+        this.sourceY = sourceY;
+        this.sourceZ = sourceZ;
+        this.sourceBlock = null;
+    }
+
+    /**
+     * Gets the source block, causing this event
+     *
+     * @return Source block
+     */
+    public Block getSourceBlock() {
+        return sourceBlock == null ? (sourceBlock = block.getWorld().getBlockAt(sourceX, sourceY, sourceZ)) : sourceBlock;
+    }
+    // Paper end
     /**
      *
      * @deprecated Magic value
@@ -22,6 +54,7 @@ public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
     public BlockPhysicsEvent(final Block block, final int changed) {
         super(block);
         this.changed = changed;
+        this.sourceBlock = block; // Paper - add source block
     }
 
     public static HandlerList getHandlerList() {
