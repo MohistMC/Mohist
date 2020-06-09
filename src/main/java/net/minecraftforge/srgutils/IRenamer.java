36d0edcb5a51fb3973ca530a3dae63e198a5acc1
Pobetore <65790947+Pobetore@users.noreply.github.com>
2020-06-09 15:42:07 +0200

    Adding Srgutils from forge, needed to work with .srg files
    //todo, add this as a gradle dependence

---
+++ src/main/java/net/minecraftforge/srgutils/IRenamer.java
@@ -0,0 +1,43 @@
+/*
+ * SRG Utils
+ * Copyright (c) 2019
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.srgutils;
+
+import net.minecraftforge.srgutils.IMappingFile.IClass;
+import net.minecraftforge.srgutils.IMappingFile.IField;
+import net.minecraftforge.srgutils.IMappingFile.IMethod;
+import net.minecraftforge.srgutils.IMappingFile.IPackage;
+
+public interface IRenamer {
+    default String rename(IPackage value) {
+        return value.getMapped();
+    }
+
+    default String rename(IClass value) {
+        return value.getMapped();
+    }
+
+    default String rename(IField value) {
+        return value.getMapped();
+    }
+
+    default String rename(IMethod value) {
+        return value.getMapped();
+    }
+}
