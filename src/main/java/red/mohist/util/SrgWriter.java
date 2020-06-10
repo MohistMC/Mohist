5245632614658f0ec35f425a33b747dbb634feef
Pobetore <65790947+Pobetore@users.noreply.github.com>
2020-06-10 11:25:50 +0200

    Update SrgWriter.java

--- src/main/java/red/mohist/util/SrgWriter.java
+++ src/main/java/red/mohist/util/SrgWriter.java
@@ -3,7 +3,6 @@ package red.mohist.util;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-
 /**
  * @Author Phobetor
  *
@@ -12,24 +11,18 @@ import java.io.IOException;
 
 public class SrgWriter {
 
-
     public void WriteCL(BufferedWriter out, String org, String mapped) throws IOException {
-        out.write("CL:"+" " + org + " "+ mapped);
+        out.write("CL:" + " " + org + " " + mapped);
         out.newLine();
-
-
-
-
-
-
     }
+
     public void WriteFD(BufferedWriter out, String org, String mapped) throws IOException {
         out.write("FD:" + " " + org + " " + mapped);
         out.newLine();
     }
 
     public void WriteMD(BufferedWriter out, String org1, String org2, String mapped1, String mapped2) throws IOException {
-        out.write("MD:" + " " + org1 + " " + org2+ " " + mapped1 + " " + mapped2);
+        out.write("MD:" + " " + org1 + " " + org2 + " " + mapped1 + " " + mapped2);
         out.newLine();
     }
 }
