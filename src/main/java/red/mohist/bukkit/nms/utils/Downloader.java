9ee24f691182cc2c7f2660c45ff6edd014a82252
MCUmbrella <mcumbrella@qq.com>
2020-06-20 22:27:17 +0800

    Optimize the response when the file download fails

--- src/main/java/red/mohist/bukkit/nms/utils/Downloader.java
+++ src/main/java/red/mohist/bukkit/nms/utils/Downloader.java
@@ -34,34 +34,43 @@ public class Downloader {
         String url = "https://files.minecraftforge.net/maven/";
         System.out.println(Message.getString("mcp.download"));
         File mcptool = new File(temppath + "/mcp.zip");
-        if(Message.isCN()) url = "http://bmclapi2.bangbang93.com/maven/";
+        if (Message.isCN()) url = "http://bmclapi2.bangbang93.com/maven/";
         String newurl = url + "de/oceanlabs/mcp/mcp_config/1.12.2-20200226.224830/mcp_config-1.12.2-20200226.224830.zip";
-        ReadableByteChannel readChannel = Channels.newChannel(new URL(newurl).openStream());
-        FileOutputStream fileOS = new FileOutputStream(mcptool);
-        System.out.println(Message.getString("mcp.extract"));
-        FileChannel writeChannel = fileOS.getChannel();
-        writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
-        ZipFile mcp = new ZipFile(mcptool);
-        ZipEntry tsrg = mcp.getEntry("config/joined.tsrg");
-        InputStream zip = mcp.getInputStream(tsrg);
-        List<String> lines = new BufferedReader(new InputStreamReader(zip, StandardCharsets.UTF_8)).lines()
-                .filter(l -> !l.isEmpty())
-                .collect(Collectors.toList());
-        FileOutputStream fos = new FileOutputStream(temppath + "/joined.tsrg");
-        BufferedWriter copy = new BufferedWriter(new OutputStreamWriter(fos));
-        for (String line : lines) {
-            copy.write(line);
-            copy.newLine();
+        try {
+            ReadableByteChannel readChannel = Channels.newChannel(new URL(newurl).openStream());
+            FileOutputStream fileOS = new FileOutputStream(mcptool);
+            System.out.println(Message.getString("mcp.extract"));
+            FileChannel writeChannel = fileOS.getChannel();
+            writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
+            ZipFile mcp = new ZipFile(mcptool);
+            ZipEntry tsrg = mcp.getEntry("config/joined.tsrg");
+            InputStream zip = mcp.getInputStream(tsrg);
+            List<String> lines = new BufferedReader(new InputStreamReader(zip, StandardCharsets.UTF_8)).lines()
+                    .filter(l -> !l.isEmpty())
+                    .collect(Collectors.toList());
+            FileOutputStream fos = new FileOutputStream(temppath + "/joined.tsrg");
+            BufferedWriter copy = new BufferedWriter(new OutputStreamWriter(fos));
+            for (String line : lines) {
+                copy.write(line);
+                copy.newLine();
+            }
+            copy.close();
+            fos.close();
+            zip.close();
+            mcp.close();
+            fileOS.close();
+            writeChannel.close();
+            mcptool.delete();
+            File partz = new File(temppath + "/joined.tsrg");
+            IMappingFile.load(partz).write(Paths.get(temppath + "/joined.srg"), IMappingFile.Format.SRG, false);
+            partz.delete();
+        } catch (Throwable e) {
+            if (Message.isCN()) {
+                System.out.println("[X] 下载Mod Coder Pack数据失败，请检查网络连接！");
+            } else {
+                System.out.println("[X] Failed to download MCP data! Please check your network connection!");
+            }
+            System.exit(-1);
         }
-        copy.close();
-        fos.close();
-        zip.close();
-        mcp.close();
-        fileOS.close();
-        writeChannel.close();
-        mcptool.delete();
-        File partz = new File(temppath + "/joined.tsrg");
-        IMappingFile.load(partz).write(Paths.get(temppath + "/joined.srg"),IMappingFile.Format.SRG, false);
-        partz.delete();
     }
 }
