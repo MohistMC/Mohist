148c484ddb90fefa95af62f7abf1b6a7956312f3
晚餐骨Dinnerbone <3293876954@qq.com>
2020-06-21 13:50:41 +0800

    modify RandomUtil bug

---
+++ src/main/java/red/mohist/util/RandomUtil.java
@@ -0,0 +1,29 @@
+package red.mohist.util;
+
+public class RandomUtil {
+
+
+
+    public void generateNoise(int[] noiseData, int rows, int cols) {
+    }
+
+    public void GenerateWhiteNoise(int width, int height) //White Gen
+    {
+    }
+
+    public void GenerateSmoothNoise(float[][] baseNoise, int octave)
+    {
+
+    }
+
+    public float Interpolate(float x0, float x1, float alpha)
+    {
+        return x0 * (1 - alpha) + alpha * x1;
+    }
+
+    public void GeneratePerlinNoise(float[][] baseNoise, int octaveCount)
+    {
+
+    }
+
+}
