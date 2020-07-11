1f42bdda995176d6a4d78485514042179cc71e9d
braddevans <braddevans123@gmail.com>
2020-07-11 17:20:56 +0100

    push fixes
    Signed-off-by: braddevans <braddevans123@gmail.com>

--- src/main/java/org/spigotmc/SneakyThrow.java
+++ src/main/java/org/spigotmc/SneakyThrow.java
@@ -3,7 +3,7 @@ package org.spigotmc;
 public class SneakyThrow {
 
     public static void sneaky(Throwable t) {
-        throw SneakyThrow.superSneaky(t);
+        throw SneakyThrow.<RuntimeException>superSneaky(t);
     }
 
     private static <T extends Throwable> T superSneaky(Throwable t) throws T {
