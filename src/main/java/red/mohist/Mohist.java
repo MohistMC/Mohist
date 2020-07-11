9e2404035028752dde6f0a19866b9411c641413d
Shawiiz_z <55449195+Shawiizz@users.noreply.github.com>
2020-07-11 14:22:45 +0200

    Changed things and fix bug

--- src/main/java/red/mohist/Mohist.java
+++ src/main/java/red/mohist/Mohist.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Logger;
 import red.mohist.bukkit.AutoDeletePlugins;
 import red.mohist.bukkit.nms.MappingFix;
 import red.mohist.configuration.MohistConfigUtil;
+import red.mohist.network.download.DownloadJava;
 import red.mohist.network.download.DownloadLibraries;
 import red.mohist.network.download.UpdateUtils;
 import red.mohist.util.i18n.Message;
@@ -25,14 +26,9 @@ public class Mohist {
   }
 
   public static void main(String[] args) throws Throwable {
-    if(Float.parseFloat(System.getProperty("java.class.version")) != 52.0) {
-      System.out.println(Message.getString("unsupported.java.version"));
-      System.exit(0);
-    }
-    if(System.getProperty("log4j.configurationFile") == null) {
-      System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
-    }
-      System.out.println("\n" +
+    if(Float.parseFloat(System.getProperty("java.class.version")) != 52.0 || bMohist("use_custom_java8", "false"))
+      DownloadJava.run(args);
+    System.out.println("\n" +
       "\n" +
       " __    __   ______   __  __   __   ______   ______  \n" +
       "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
@@ -42,28 +38,19 @@ public class Mohist {
       "                                                    \n" +
       "\n");
     System.out.println("                                      " + Message.getString("forge.serverlanunchwrapper.1"));
-    if(System.getProperty("log4j.configurationFile") == null) {
+    if(System.getProperty("log4j.configurationFile") == null)
       System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
-    }
-    if(bMohist("check_libraries")) {
-      DownloadLibraries.run();
-    }
-    MappingFix.init();
+    if(bMohist("check_libraries")) DownloadLibraries.run();
     MohistConfigUtil.copyMohistConfig();
+    MappingFix.init();
     if(!hasAcceptedEULA()) {
       System.out.println(Message.getString("eula"));
       while (!"true".equals(new Scanner(System.in).next())) ;
       writeInfos();
     }
-    if(bMohist("check_update")) {
-      UpdateUtils.versionCheck();
-    }
-    if(!bMohist("disable_plugins_blacklist")) {
-      AutoDeletePlugins.jar();
-    }
-    if(!bMohist("disable_mods_blacklist")) {
-      jar((byte) 1);
-    }
+    if(bMohist("check_update")) UpdateUtils.versionCheck();
+    if(!bMohist("disable_plugins_blacklist")) AutoDeletePlugins.jar();
+    if(!bMohist("disable_mods_blacklist")) jar((byte) 1);
     jar((byte) 2);
 
     Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper").getDeclaredMethod("main", String[].class).invoke(null, new Object[]{args});
