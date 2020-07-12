dc93844ee6c7d141c0a1740f1afe9e670ff2935d
晚餐骨Dinnerbone <3293876954@qq.com>
2020-07-13 06:52:44 +0800

    Fixed can't start server bug pls use this commit!!!Please go to Jenkins to use the latest version, you need to regenerate the server.properties file

--- src/main/java/red/mohist/Mohist.java
+++ src/main/java/red/mohist/Mohist.java
@@ -1,56 +1,78 @@
 package red.mohist;
 
 import org.apache.logging.log4j.Logger;
+
 import red.mohist.bukkit.AutoDeletePlugins;
 import red.mohist.bukkit.nms.MappingFix;
+
 import red.mohist.configuration.MohistConfigUtil;
+import static red.mohist.configuration.MohistConfigUtil.bMohist;
+import static red.mohist.forge.AutoDeleteMods.jar;
+
 import red.mohist.network.download.DownloadJava;
 import red.mohist.network.download.DownloadLibraries;
 import red.mohist.network.download.UpdateUtils;
+import static red.mohist.util.EulaUtil.hasAcceptedEULA;
+import static red.mohist.util.EulaUtil.writeInfos;
 import red.mohist.util.i18n.Message;
 
 import java.util.Scanner;
-
-import static red.mohist.configuration.MohistConfigUtil.bMohist;
-import static red.mohist.forge.AutoDeleteMods.jar;
-import static red.mohist.util.EulaUtil.hasAcceptedEULA;
-import static red.mohist.util.EulaUtil.writeInfos;
-
 public class Mohist {
+    public static final String NAME = "Mohist";
+    public static Logger LOGGER;
 
-  public static final String NAME = "Mohist";
-  public static Logger LOGGER;
-
-  public static String getVersion() {
-    return Mohist.class.getPackage().getImplementationVersion() != null ? Metrics.class.getPackage().getImplementationVersion() : "unknown";
-  }
-
-  public static void main(String[] args) throws Throwable {
-    MohistConfigUtil.copyMohistConfig();
-    System.out.println("\n" +
-      "\n" +
-      " __    __   ______   __  __   __   ______   ______  \n" +
-      "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
-      "\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
-      " \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
-      "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
-      "                                                    \n" +
-      "\n");
-    System.out.println("                                      " + Message.getString("forge.serverlanunchwrapper.1"));
-    if(System.getProperty("log4j.configurationFile") == null)
-      System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
-    if(bMohist("check_libraries")) DownloadLibraries.run();
-    MappingFix.init();
-    if(!hasAcceptedEULA()) {
-      System.out.println(Message.getString("eula"));
-      while (!"true".equals(new Scanner(System.in).next())) ;
-      writeInfos();
+    public static String getVersion() {
+        return (Mohist.class.getPackage().getImplementationVersion() != null)
+                ? Metrics.class.getPackage().getImplementationVersion() : "unknown";
     }
-    if(bMohist("check_update")) UpdateUtils.versionCheck();
-    if(!bMohist("disable_plugins_blacklist")) AutoDeletePlugins.jar();
-    if(!bMohist("disable_mods_blacklist")) jar((byte) 1);
-    jar((byte) 2);
 
-    Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper").getDeclaredMethod("main", String[].class).invoke(null, new Object[]{args});
-  }
+    public static void main(String[] args) throws Throwable {
+        MohistConfigUtil.copyMohistConfig();
+        System.out.println("\n" + "\n" +
+                " __    __   ______   __  __   __   ______   ______  \n" +
+                "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
+                "\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
+                " \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
+                "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
+                "                                                    \n" + "\n");
+        System.out.println("                                      " +
+                Message.getString("forge.serverlanunchwrapper.1"));
+
+        if (System.getProperty("log4j.configurationFile") == null) {
+            System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
+        }
+
+        if (bMohist("check_libraries")) {
+            DownloadLibraries.run();
+        }
+
+        MappingFix.init();
+
+        if (!hasAcceptedEULA()) {
+            System.out.println(Message.getString("eula"));
+
+            while (!"true".equals(new Scanner(System.in).next()))
+                ;
+
+            writeInfos();
+        }
+
+        if (bMohist("check_update")) {
+            UpdateUtils.versionCheck();
+        }
+
+        if (!bMohist("disable_plugins_blacklist")) {
+            AutoDeletePlugins.jar();
+        }
+
+        if (!bMohist("disable_mods_blacklist")) {
+            jar((byte) 1);
+        }
+
+        jar((byte) 2);
+
+        Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper")
+                .getDeclaredMethod("main", String[].class)
+                .invoke(null, new Object[] { args });
+    }
 }
