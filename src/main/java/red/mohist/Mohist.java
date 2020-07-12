f1df30ae088e27469f810679cfe7c31464a051e5
晚餐骨Dinnerbone <3293876954@qq.com>
2020-07-12 22:24:49 +0800

    Fixed can't start server bug pls use this commit!!!

--- src/main/java/red/mohist/Mohist.java
+++ src/main/java/red/mohist/Mohist.java
@@ -37,7 +37,6 @@ public class Mohist {
       "                                                    \n" +
       "\n");
     System.out.println("                                      " + Message.getString("forge.serverlanunchwrapper.1"));
-    Mohist.LOGGER.info(Message.getString("properties.java.version") + System.getProperty("java.version"));
     if(System.getProperty("log4j.configurationFile") == null)
       System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
     if(bMohist("check_libraries")) DownloadLibraries.run();
