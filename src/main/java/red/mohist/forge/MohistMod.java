dd6aef491c80aa94c609f1f91f855f013fe6ee14
Administrator <1798643961@qq.com>
2020-07-27 21:20:29 +0800

    Fix the bug

--- src/main/java/red/mohist/forge/MohistMod.java
+++ src/main/java/red/mohist/forge/MohistMod.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import red.mohist.bukkit.BukkitPermissionsHandler;
-import red.mohist.forge.registry.BukkitRegistry;
 
 @Mod("mohist")
 public class MohistMod {
@@ -20,8 +19,6 @@ public class MohistMod {
         LOGGER.info("mod loaded...");
         PermissionAPI.setPermissionHandler(new BukkitPermissionsHandler());
         MohistMod.LOGGER.info("Registered Forge API Permission Handler");
-        BukkitRegistry.registerAll();
-        MohistMod.LOGGER.info("Registered Bukkit");
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
