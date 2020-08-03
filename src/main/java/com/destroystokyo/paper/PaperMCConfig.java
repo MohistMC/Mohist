f02deab30f32c439a4a95a73c48e1c183cf640c9
Mgazul <mgazul@163.com>
2020-08-04 00:04:46 +0800

    Fixed an incorrect patch

--- src/main/java/com/destroystokyo/paper/PaperMCConfig.java
+++ src/main/java/com/destroystokyo/paper/PaperMCConfig.java
@@ -311,10 +311,4 @@ public class PaperMCConfig {
             maxBookTotalSizeMultiplier = 0.98D;
         }
     }
-
-    public static boolean allowPistonDuplication;
-    private static void allowPistonDuplication() {
-        allowPistonDuplication = getBoolean("settings.unsupported-settings.allow-piston-duplication", config.getBoolean("settings.unsupported-settings.allow-tnt-duplication", false));
-        set("settings.unsupported-settings.allow-tnt-duplication", null);
-    }
 }
