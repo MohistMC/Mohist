ce4aee76623fb663b27c62eb84e7b979fd27812b
Mgazul <mgazul@163.com>
2020-08-11 12:39:49 +0800

    Update Downloader.java

--- src/main/java/red/mohist/bukkit/nms/utils/Downloader.java
+++ src/main/java/red/mohist/bukkit/nms/utils/Downloader.java
@@ -16,27 +16,27 @@ import red.mohist.util.i18n.Message;
 
 public class Downloader {
 
-    public void execute(String temppath) {
-      String url = "https://raw.githubusercontent.com/Shawiizz/shawiizz.github.io/master/";
-      if(Message.isCN()) url = "http://bmclapi2.bangbang93.com/maven/";
-      System.out.println(Message.getString("mcp.download"));
-      File mcptool = new File(temppath + "/mcp.zip");
-      File partz = new File(temppath + "/joined.tsrg");
-      String newurl = url + "de/oceanlabs/mcp/mcp_config/1.12.2-20200226.224830/mcp_config-1.12.2-20200226.224830.zip";
-      try {
-        UpdateUtils.downloadFile(newurl, mcptool);
-        System.out.println(Message.getString("mcp.extract"));
-        ZipFile mcp = new ZipFile(mcptool);
-        Files.copy(mcp.getInputStream(mcp.getEntry("config/joined.tsrg")), partz.toPath());
-        mcp.close();
-        IMappingFile.load(partz).write(Paths.get(temppath + "/joined.srg"), IMappingFile.Format.SRG, false);
-        System.gc();
-        Thread.sleep(100);
-        mcptool.delete();
-        partz.delete();
-      } catch (Throwable e) {
-        System.out.println(Message.getString("mcp.dl.fail"));
-        System.exit(-1);
-      }
+  public void execute(String temppath) {
+    String url = "https://raw.githubusercontent.com/Shawiizz/shawiizz.github.io/master/";
+    if (Message.isCN()) url = "https://files.minecraftforge.net/maven/";
+    System.out.println(Message.getString("mcp.download"));
+    File mcptool = new File(temppath + "/mcp.zip");
+    File partz = new File(temppath + "/joined.tsrg");
+    String newurl = url + "de/oceanlabs/mcp/mcp_config/1.12.2-20200226.224830/mcp_config-1.12.2-20200226.224830.zip";
+    try {
+      UpdateUtils.downloadFile(newurl, mcptool);
+      System.out.println(Message.getString("mcp.extract"));
+      ZipFile mcp = new ZipFile(mcptool);
+      Files.copy(mcp.getInputStream(mcp.getEntry("config/joined.tsrg")), partz.toPath());
+      mcp.close();
+      IMappingFile.load(partz).write(Paths.get(temppath + "/joined.srg"), IMappingFile.Format.SRG, false);
+      System.gc();
+      Thread.sleep(100);
+      mcptool.delete();
+      partz.delete();
+    } catch (Throwable e) {
+      System.out.println(Message.getString("mcp.dl.fail"));
+      System.exit(-1);
     }
+  }
 }
