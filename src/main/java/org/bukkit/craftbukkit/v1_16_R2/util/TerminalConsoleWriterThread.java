74e725617df51fb43d3ccc092925edfbb2e0fd78
Mgazul <mgazul@163.com>
2020-10-13 14:44:15 +0800

    Fix console color

--- src/main/java/org/bukkit/craftbukkit/v1_16_R2/util/TerminalConsoleWriterThread.java
+++ src/main/java/org/bukkit/craftbukkit/v1_16_R2/util/TerminalConsoleWriterThread.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jline.console.ConsoleReader;
-import org.bukkit.craftbukkit.v1_16_R2.Main;
+import org.bukkit.craftbukkit.Main;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.Ansi.Erase;
 
