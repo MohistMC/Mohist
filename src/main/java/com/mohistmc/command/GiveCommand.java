b5aae3190671db118e7ead093975508e24879a29
Mgazul <mgazul@163.com>
2020-11-05 16:52:46 +0800

    Fix npe

--- src/main/java/com/mohistmc/command/GiveCommand.java
+++ src/main/java/com/mohistmc/command/GiveCommand.java
@@ -13,43 +13,41 @@ public class GiveCommand {
 
     public static void info(CommandSender sender, String[] args) {
         if (args.length >= 3) {
-            String playername = null;
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (player.getName().toLowerCase().equals(args[1])) {
-                    playername = player.getName();
-                }
-            }
-            Player player = Bukkit.getPlayer(playername);
-            if (player == null) return;
-            String args2 = args[2];
-            String[] s = args2.split(":");
-            String j = args2.contains(":") ? s[1] : String.valueOf(0);
-            // mohist give Mgazul 4096 64
-            if (NumberUtils.isInteger(args2)) {
-                int id = Integer.valueOf(args2).intValue();
-                if (Material.byId[id] != null) {
-                    int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
-                    ItemStack itemStack = new ItemStack(Material.getMaterial(id), i, Short.parseShort(j));
-                    if (itemStack != null) {
-                        player.getInventory().addItem(itemStack);
+            for (Player player0 : Bukkit.getOnlinePlayers()) {
+                if (player0.getName().toLowerCase().equals(args[1])) {
+                    Player player = Bukkit.getPlayer(args[1]);
+                    if (player == null) return;
+                    String args2 = args[2];
+                    String[] s = args2.split(":");
+                    String j = args2.contains(":") ? s[1] : String.valueOf(0);
+                    // mohist give Mgazul 4096 64
+                    if (NumberUtils.isInteger(args2)) {
+                        int id = Integer.valueOf(args2).intValue();
+                        if (Material.byId[id] != null) {
+                            int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
+                            ItemStack itemStack = new ItemStack(Material.getMaterial(id), i, Short.parseShort(j));
+                            if (itemStack != null) {
+                                player.getInventory().addItem(itemStack);
+                            }
+                        }
                     }
-                }
-            }
-            // mohist give Mgazul 4096:1 64
-            else if (args2.contains(":") && NumberUtils.isInteger(s[0]) && NumberUtils.isInteger(s[1])) {
-                int id = Integer.valueOf(s[0]).intValue();
-                int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
-                ItemStack itemStack = new ItemStack(Material.getMaterial(id), i, Short.parseShort(s[1]));
-                if (itemStack != null) {
-                    player.getInventory().addItem(itemStack);
-                }
-            }
-            // mohist give Mgazul ASDASD 64
-            else if (Material.getMaterial(args2) != null) {
-                int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
-                ItemStack itemStack = new ItemStack(Material.getMaterial(args2), i, Short.parseShort(j));
-                if (itemStack != null) {
-                    player.getInventory().addItem(itemStack);
+                    // mohist give Mgazul 4096:1 64
+                    else if (args2.contains(":") && NumberUtils.isInteger(s[0]) && NumberUtils.isInteger(s[1])) {
+                        int id = Integer.valueOf(s[0]).intValue();
+                        int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
+                        ItemStack itemStack = new ItemStack(Material.getMaterial(id), i, Short.parseShort(s[1]));
+                        if (itemStack != null) {
+                            player.getInventory().addItem(itemStack);
+                        }
+                    }
+                    // mohist give Mgazul ASDASD 64
+                    else if (Material.getMaterial(args2) != null) {
+                        int i = args.length == 4 ? Integer.parseInt(args[3]) : 1;
+                        ItemStack itemStack = new ItemStack(Material.getMaterial(args2), i, Short.parseShort(j));
+                        if (itemStack != null) {
+                            player.getInventory().addItem(itemStack);
+                        }
+                    }
                 }
             }
         }
