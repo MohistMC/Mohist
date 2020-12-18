dca9aa76baeba01d76ca136807242073a821de46
Mgazul <mgazul@163.com>
2020-12-18 18:54:04 +0800

    Enhanced mod compatibility

--- src/main/java/org/bukkit/craftbukkit/v1_16_R3/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_16_R3/event/CraftEventFactory.java
@@ -922,8 +922,9 @@ public class CraftEventFactory {
 
         if (cause != null) {
             return callEntityDamageEvent(null, entity, cause, modifiers, modifierFunctions, cancelled);
+        } else {
+            return new EntityDamageEvent(entity.getBukkitEntity(), DamageCause.CUSTOM, modifiers, modifierFunctions);
         }
-        throw new IllegalStateException(String.format("Unhandled damage of %s from %s", entity, source.damageType));
     }
 
     private static EntityDamageEvent callEntityDamageEvent(Entity damager, Entity damagee, DamageCause cause, Map<DamageModifier, Double> modifiers, Map<DamageModifier, Function<? super Double, Double>> modifierFunctions) {
