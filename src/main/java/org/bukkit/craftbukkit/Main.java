6660bf6382c4a05c9d146fdc8402e11117b49883
Mgazul <mgazul@163.com>
2020-12-25 01:07:20 +0800

    Fix

--- src/main/java/org/bukkit/craftbukkit/Main.java
+++ src/main/java/org/bukkit/craftbukkit/Main.java
@@ -17,180 +17,108 @@ import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
 
 public class Main extends OptionParser {
 
-    public static boolean useJline = true;
-    public static boolean useConsole = true;
+    public Main() {
+        acceptsAll(asList("c", "config"), "Properties file to use")
+                .withRequiredArg()
+                .ofType(File.class)
+                .defaultsTo(new File("server.properties"))
+                .describedAs("Properties file");
 
-    public static OptionSet main(String[] args) {
-        // Todo: Installation script
-        OptionParser parser = new OptionParser() {
-            {
-                acceptsAll(asList("gameDir"))
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File(".")); //Forge: Consume this argument, we use it in the launcher,
+        acceptsAll(asList("P", "plugins"), "Plugin directory to use")
+                .withRequiredArg()
+                .ofType(File.class)
+                .defaultsTo(new File("plugins"))
+                .describedAs("Plugin directory");
 
-                acceptsAll(asList("?", "help"), "Show the help");
+        acceptsAll(asList("h", "host", "server-ip"), "Host to listen on")
+                .withRequiredArg()
+                .ofType(String.class)
+                .describedAs("Hostname or IP");
 
-                acceptsAll(asList("c", "config"), "Properties file to use")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("server.properties"))
-                        .describedAs("Properties file");
+        acceptsAll(asList("W", "world-dir", "universe", "world-container"), "World container")
+                .withRequiredArg()
+                .ofType(File.class)
+                .describedAs("Directory containing worlds");
 
-                acceptsAll(asList("P", "plugins"), "Plugin directory to use")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("plugins"))
-                        .describedAs("Plugin directory");
+        acceptsAll(asList("w", "world", "level-name"), "World name")
+                .withRequiredArg()
+                .ofType(String.class)
+                .describedAs("World name");
 
-                acceptsAll(asList("h", "host", "server-ip"), "Host to listen on")
-                        .withRequiredArg()
-                        .ofType(String.class)
-                        .describedAs("Hostname or IP");
+        acceptsAll(asList("p", "port", "server-port"), "Port to listen on")
+                .withRequiredArg()
+                .ofType(Integer.class)
+                .describedAs("Port");
 
-                acceptsAll(asList("W", "world-dir", "universe", "world-container"), "World container")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("."))
-                        .describedAs("Directory containing worlds");
+        acceptsAll(asList("o", "online-mode"), "Whether to use online authentication")
+                .withRequiredArg()
+                .ofType(Boolean.class)
+                .describedAs("Authentication");
 
-                acceptsAll(asList("w", "world", "level-name"), "World name")
-                        .withRequiredArg()
-                        .ofType(String.class)
-                        .describedAs("World name");
+        acceptsAll(asList("s", "size", "max-players"), "Maximum amount of players")
+                .withRequiredArg()
+                .ofType(Integer.class)
+                .describedAs("Server size");
 
-                acceptsAll(asList("p", "port", "server-port"), "Port to listen on")
-                        .withRequiredArg()
-                        .ofType(Integer.class)
-                        .describedAs("Port");
+        acceptsAll(asList("d", "date-format"), "Format of the date to display in the console (for log entries)")
+                .withRequiredArg()
+                .ofType(SimpleDateFormat.class)
+                .describedAs("Log date format");
 
-                acceptsAll(asList("o", "online-mode"), "Whether to use online authentication")
-                        .withRequiredArg()
-                        .ofType(Boolean.class)
-                        .describedAs("Authentication");
+        acceptsAll(asList("log-pattern"), "Specfies the log filename pattern")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("server.log")
+                .describedAs("Log filename");
 
-                acceptsAll(asList("s", "size", "max-players"), "Maximum amount of players")
-                        .withRequiredArg()
-                        .ofType(Integer.class)
-                        .describedAs("Server size");
+        acceptsAll(asList("log-limit"), "Limits the maximum size of the log file (0 = unlimited)")
+                .withRequiredArg()
+                .ofType(Integer.class)
+                .defaultsTo(0)
+                .describedAs("Max log size");
 
-                acceptsAll(asList("d", "date-format"), "Format of the date to display in the console (for log entries)")
-                        .withRequiredArg()
-                        .ofType(SimpleDateFormat.class)
-                        .describedAs("Log date format");
+        acceptsAll(asList("log-count"), "Specified how many log files to cycle through")
+                .withRequiredArg()
+                .ofType(Integer.class)
+                .defaultsTo(1)
+                .describedAs("Log count");
 
-                acceptsAll(asList("log-pattern"), "Specfies the log filename pattern")
-                        .withRequiredArg()
-                        .ofType(String.class)
-                        .defaultsTo("server.log")
-                        .describedAs("Log filename");
+        acceptsAll(asList("log-append"), "Whether to append to the log file")
+                .withRequiredArg()
+                .ofType(Boolean.class)
+                .defaultsTo(true)
+                .describedAs("Log append");
 
-                acceptsAll(asList("log-limit"), "Limits the maximum size of the log file (0 = unlimited)")
-                        .withRequiredArg()
-                        .ofType(Integer.class)
-                        .defaultsTo(0)
-                        .describedAs("Max log size");
+        acceptsAll(asList("log-strip-color"), "Strips color codes from log file");
 
-                acceptsAll(asList("log-count"), "Specified how many log files to cycle through")
-                        .withRequiredArg()
-                        .ofType(Integer.class)
-                        .defaultsTo(1)
-                        .describedAs("Log count");
+        acceptsAll(asList("b", "bukkit-settings"), "File for bukkit settings")
+                .withRequiredArg()
+                .ofType(File.class)
+                .defaultsTo(new File("bukkit.yml"))
+                .describedAs("Yml file");
 
-                acceptsAll(asList("log-append"), "Whether to append to the log file")
-                        .withRequiredArg()
-                        .ofType(Boolean.class)
-                        .defaultsTo(true)
-                        .describedAs("Log append");
+        acceptsAll(asList("C", "commands-settings"), "File for command settings")
+                .withRequiredArg()
+                .ofType(File.class)
+                .defaultsTo(new File("commands.yml"))
+                .describedAs("Yml file");
 
-                acceptsAll(asList("log-strip-color"), "Strips color codes from log file");
+        acceptsAll(asList("forceUpgrade"), "Whether to force a world upgrade");
+        acceptsAll(asList("eraseCache"), "Whether to force cache erase during world upgrade");
 
-                acceptsAll(asList("b", "bukkit-settings"), "File for bukkit settings")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("bukkit.yml"))
-                        .describedAs("Yml file");
+        acceptsAll(asList("nojline"), "Disables jline and emulates the vanilla console");
 
-                acceptsAll(asList("C", "commands-settings"), "File for command settings")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("commands.yml"))
-                        .describedAs("Yml file");
+        acceptsAll(asList("noconsole"), "Disables the console");
 
-                acceptsAll(asList("forceUpgrade"), "Whether to force a world upgrade");
-                acceptsAll(asList("eraseCache"), "Whether to force cache erase during world upgrade");
-                acceptsAll(asList("nogui"), "Disables the graphical console");
+        acceptsAll(asList("demo"), "Demo mode");
 
-                acceptsAll(asList("nojline"), "Disables jline and emulates the vanilla console");
+        // Spigot Start
+        acceptsAll(asList("S", "spigot-settings"), "File for spigot settings")
+                .withRequiredArg()
+                .ofType(File.class)
+                .defaultsTo(new File("spigot.yml"))
+                .describedAs("Yml file");
 
-                acceptsAll(asList("noconsole"), "Disables the console");
-
-                acceptsAll(asList("v", "version"), "Show the CraftBukkit Version");
-
-                acceptsAll(asList("demo"), "Demo mode");
-
-                // Spigot Start
-                acceptsAll(asList("S", "spigot-settings"), "File for spigot settings")
-                        .withRequiredArg()
-                        .ofType(File.class)
-                        .defaultsTo(new File("spigot.yml"))
-                        .describedAs("Yml file");
-                // Spigot End
-            }
-        };
-
-        OptionSet options = null;
-
-        try {
-            options = parser.parse(args);
-        } catch (joptsimple.OptionException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, ex.getLocalizedMessage());
-        }
-
-        if ((options == null) || (options.has("?"))) {
-            try {
-                parser.printHelpOn(System.out);
-            } catch (IOException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else if (options.has("v")) {
-            System.out.println(CraftServer.class.getPackage().getImplementationVersion());
-        } else {
-            // Do you love Java using + and ! as string based identifiers? I sure do!
-            String path = new File(".").getAbsolutePath();
-            if (path.contains("!") || path.contains("+")) {
-                System.err.println(Message.get("error.start.directory"));
-                return null;
-            }
-
-            float javaVersion = Float.parseFloat(System.getProperty("java.class.version"));
-            if (javaVersion > 59.0) {
-                System.err.println(Message.get("unsupport.java", javaVersion));
-                return null;
-            }
-
-            try {
-                if (options.has("nojline")) {
-                    System.setProperty(TerminalConsoleAppender.JLINE_OVERRIDE_PROPERTY, "false");
-                    useJline = false;
-                }
-                if (options.has("noconsole")) {
-                    useConsole = false;
-                    useJline = false; // Paper
-                    System.setProperty(TerminalConsoleAppender.JLINE_OVERRIDE_PROPERTY, "false");
-                }
-
-                System.out.println(Message.get("load.libraries"));
-                // net.minecraft.server.Main.main(options);
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-            return options;
-        }
-        return null;
-    }
-
-    private static List<String> asList(String... params) {
-        return Arrays.asList(params);
+        allowsUnrecognizedOptions();
     }
 }
