0a55383050858d059a11569b726631a25fd417fe
Mgazul <mgazul@163.com>
2021-01-15 14:14:40 +0800

    #642

---
+++ src/main/java/com/mohistmc/eventhandler/EventDispatcherRegistry.java
@@ -0,0 +1,12 @@
+package com.mohistmc.eventhandler;
+
+import com.mohistmc.MohistMC;
+import net.minecraftforge.common.MinecraftForge;
+
+public class EventDispatcherRegistry {
+
+    public static void init() {
+        MinecraftForge.EVENT_BUS.register(new BlockEventDispatcher());
+        MohistMC.LOGGER.info("EventDispatcherRegistry initialize");
+    }
+}
