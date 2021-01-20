b88044c32567a48c766094935cc2f9381020c4e4
Mgazul <mgazul@163.com>
2021-01-20 21:17:43 +0800

    Use mohist api for new version check

--- src/main/java/com/mohistmc/network/download/NetworkUtil.java
+++ src/main/java/com/mohistmc/network/download/NetworkUtil.java
@@ -12,6 +12,8 @@ public class NetworkUtil {
         try {
             conn = new URL(URL).openConnection();
             conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0");
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
         } catch (IOException e) {
             e.printStackTrace();
         }
