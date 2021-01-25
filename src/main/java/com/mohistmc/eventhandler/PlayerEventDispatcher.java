82842daa449f24e6ac959c1fb0ddf68413695797
Mgazul <mgazul@163.com>
2021-01-25 13:30:09 +0800

    biu

---
+++ src/main/java/com/mohistmc/eventhandler/PlayerEventDispatcher.java
@@ -0,0 +1,18 @@
+package com.mohistmc.eventhandler;
+
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.event.entity.player.AdvancementEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+public class PlayerEventDispatcher {
+
+    @SubscribeEvent
+    public void onAdvancementDone(AdvancementEvent event) {
+        if (event.getPlayer() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+            Bukkit.getPluginManager().callEvent(new PlayerAdvancementDoneEvent(player.getBukkitEntity(), event.getAdvancement().bukkit));
+        }
+    }
+}
