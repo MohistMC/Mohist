29955d1e6e4007c58d4196b3562ccced5251b3ae
BuildTools <unconfigured@null.spigotmc.org>
2021-02-05 22:15:04 +0800

    Better handler PlayerInteractEvent

--- src/main/java/com/mohistmc/eventhandler/BlockEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/BlockEventDispatcher.java
@@ -1,18 +1,23 @@
 package com.mohistmc.eventhandler;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_16_R3.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_16_R3.event.CraftEventFactory;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
 
 public class BlockEventDispatcher {
 
     public static boolean isDropItems;
 
+    //For BlockBreakEvent
     @SubscribeEvent(receiveCanceled = true)
-    public void onBreakBlock(BlockEvent.BreakEvent event) {
+    public void onBreakBlockEvent(BlockEvent.BreakEvent event) {
         if (!event.getWorld().isRemote()) {
             CraftBlock craftBlock = CraftBlock.at(event.getWorld(), event.getPos());
             BlockBreakEvent breakEvent = new BlockBreakEvent(craftBlock, ((ServerPlayerEntity) event.getPlayer()).getBukkitEntity());
@@ -22,6 +27,26 @@ public class BlockEventDispatcher {
             event.setCanceled(breakEvent.isCancelled());
             event.setExpToDrop(breakEvent.getExpToDrop());
             isDropItems = breakEvent.isDropItems();
+        }
+    }
+
+    //For PlayerInteractEvent
+    @SubscribeEvent(receiveCanceled = true)
+    public void onTrampleFarmlandEvent(BlockEvent.FarmlandTrampleEvent event){
+        // CraftBukkit start - Interact soil
+        org.bukkit.event.Cancellable cancellable = null;
+        if (event.getEntity() instanceof PlayerEntity) {
+            cancellable = CraftEventFactory.callPlayerInteractEvent((PlayerEntity) event.getEntity(), org.bukkit.event.block.Action.PHYSICAL, event.getPos(), null, null, null);
+        } else {
+            cancellable = new EntityInteractEvent(event.getEntity().getBukkitEntity(), event.getWorld().getMinecraftWorld().getCBWorld().getBlockAt(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ()));
+            event.getWorld().getMinecraftWorld().getCBServer().getPluginManager().callEvent((EntityInteractEvent) cancellable);
+            if (cancellable != null && cancellable.isCancelled()) {
+                return;
+            }
+            if (CraftEventFactory.callEntityChangeBlockEvent(event.getEntity(), event.getPos(), Blocks.DIRT.getDefaultState()).isCancelled()) {
+                return;
+            }
+            // CraftBukkit end
         }
     }
 }
