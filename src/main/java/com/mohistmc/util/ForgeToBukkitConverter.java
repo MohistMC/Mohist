c2e369e757e210351771fe63957f5a97dee0a10c
Nicolas Went <nicolas.went@epita.fr>
2021-03-22 13:14:08 +0100

    [Util] WIP on a converter from forge ItemStack into Bukkit ItemStack

---
+++ src/main/java/com/mohistmc/util/ForgeToBukkitConverter.java
@@ -0,0 +1,22 @@
+package com.mohistmc.util;
+
+import com.mohistmc.api.ItemAPI;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * This class allow us to do some conversions from forge to bukkit.
+ */
+public final class ForgeToBukkitConverter {
+
+    private ForgeToBukkitConverter() {
+    }
+
+    /**
+     * This method allow us to convert a forge ItemStack to a bukkit ItemStack
+     * @param forgetItem The forge item to convert
+     * @return A Bukkit ItemStack corresponding to the forge one
+     */
+    public static org.bukkit.inventory.ItemStack forgeToBukkitItemStack(net.minecraft.item.ItemStack forgetItem) {
+        // TODO
+    }
+}
