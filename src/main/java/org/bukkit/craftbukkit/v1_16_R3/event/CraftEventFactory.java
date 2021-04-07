f2511b13c4734ddc91de55e899471e6297094a15
Mgazul <mgazul@163.com>
2021-04-07 16:41:27 +0800

    Updated Upstream(bukkit/craftbukkit/spigot)

--- src/main/java/org/bukkit/craftbukkit/v1_16_R3/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_16_R3/event/CraftEventFactory.java
@@ -1301,7 +1301,7 @@ public class CraftEventFactory {
     }
 
     public static ItemStack handleEditBookEvent(ServerPlayerEntity player, int itemInHandIndex, ItemStack itemInHand, ItemStack newBookItem) {
-        PlayerEditBookEvent editBookEvent = new PlayerEditBookEvent(player.getBukkitEntity(), itemInHandIndex, (BookMeta) CraftItemStack.getItemMeta(itemInHand), (BookMeta) CraftItemStack.getItemMeta(newBookItem), newBookItem.getItem() == Items.WRITTEN_BOOK);
+        PlayerEditBookEvent editBookEvent = new PlayerEditBookEvent(player.getBukkitEntity(), (itemInHandIndex >= 0 && itemInHandIndex <= 8) ? itemInHandIndex : -1, (BookMeta) CraftItemStack.getItemMeta(itemInHand), (BookMeta) CraftItemStack.getItemMeta(newBookItem), newBookItem.getItem() == Items.WRITTEN_BOOK);
         player.level.getCBServer().getPluginManager().callEvent(editBookEvent);
 
         // If they've got the same item in their hand, it'll need to be updated.
