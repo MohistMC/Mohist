252d255a8e0be4f9355a66ee8eb8f15116257dbd
墨水瓶Official <77919405+InkerBot@users.noreply.github.com>
2021-04-27 02:58:46 +0800

    Revert "add paper api patch: 0015-Expose-server-CommandMap"
    This reverts commit 9e46187707de8474610e667389c20947ddd8c4e6.

--- src/main/java/org/bukkit/Server.java
+++ src/main/java/org/bukkit/Server.java
@@ -1192,18 +1192,6 @@ public interface Server extends PluginMessageRecipient {
     @Nullable
     Entity getEntity(@NotNull UUID uuid);
 
-    // Paper start
-
-    /**
-     * Gets the active {@link org.bukkit.command.CommandMap}
-     *
-     * @return the active command map
-     */
-    @NotNull
-    org.bukkit.command.CommandMap getCommandMap();
-
-    // Paper end
-
     /**
      * Get the advancement specified by this key.
      *
