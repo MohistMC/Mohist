abd8c739acb74f35736005a56baea0d89f422687
InkerBot <2924657226@qq.com>
2021-05-23 20:14:34 +0800

    Fix #1298

--- src/main/java/com/mohistmc/entity/CraftCustomFakePlayer.java
+++ src/main/java/com/mohistmc/entity/CraftCustomFakePlayer.java
@@ -1,10 +1,15 @@
 package com.mohistmc.entity;
 
+import com.mohistmc.MohistMC;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.util.FakePlayerFactory;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class CraftCustomFakePlayer extends CraftPlayer {
 
@@ -20,5 +25,24 @@ public class CraftCustomFakePlayer extends CraftPlayer {
 
     @Override
     public void setOp(boolean value) {
+        MohistMC.LOGGER.warn(new Exception("Plugin attempt to setOp on FakePlayer."));
+    }
+
+    @Override
+    @NotNull
+    public GameMode getGameMode() {
+        // If handle has gamemode, return it
+        GameMode superGameMode = super.getGameMode();
+        if (superGameMode != null) {
+            return superGameMode;
+        }
+
+        // If It has owner, Use owner's gamemode
+        Player owner = Bukkit.getPlayer(getUniqueId());
+        if (owner != null) {
+            superGameMode = owner.getGameMode();
+        }
+
+        return superGameMode == null ? Bukkit.getServer().getDefaultGameMode() : superGameMode;
     }
 }
