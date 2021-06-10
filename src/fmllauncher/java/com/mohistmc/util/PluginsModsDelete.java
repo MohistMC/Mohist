04270a57b30f5de902409b524ad682c790c70657
Shawiiz_z <shaweereso@gmail.com>
2021-06-10 21:32:03 +0200

    Rework Plugin fix/deleter code

--- src/fmllauncher/java/com/mohistmc/util/PluginsModsDelete.java
+++ src/fmllauncher/java/com/mohistmc/util/PluginsModsDelete.java
@@ -6,135 +6,102 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 
 import static com.mohistmc.network.download.UpdateUtils.downloadFile;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class PluginsModsDelete {
+	public static final String NOT_COMPATIBLE = "not_compatible";
+	public static final String FIX = "fix";
+	public static final File PLUGIN = new File("plugins");
+	public static final File MOD = new File("mods");
 
-    public static void chekPlugins(Fix fix) throws Exception {
-        File plugins = new File("plugins");
-        String mainClassSplit = fix.mainClass.replaceAll("\\.", "/") + ".class";
-        if (!plugins.exists())
-            plugins.mkdir();
-        for (File file : Objects.requireNonNull(plugins.listFiles((dir, name) -> name.endsWith(".jar")))) {
-            if (!fileExists(file, "plugin.yml") && !fileExists(file, mainClassSplit)) return;
+	public static void checkPlugins(ArrayList<Fix> fixes, File plugins) throws Exception {
+		if(!plugins.exists() || fixes.size() == 0) return;
 
-            JarFile jarFile = new JarFile(file);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(jarFile.getInputStream(new ZipEntry("plugin.yml"))));
+		for (File pom : plugins.listFiles((dir, name) -> name.endsWith(".jar"))) {
+			ArrayList<String> entries = jarEntries(pom);
+			if(entries == null) continue;
+			String allLines = "";
 
-            boolean versionFound = false;
-            boolean mainFound = false;
+			if(plugins.equals(PLUGIN)) {
+				if(!entries.contains("plugin.yml")) {
+					System.out.println("[WARN] Plugin with jar name " + pom.getName() + " doesn't contains any plugin.yml, the server may crash or the plugin will not load properly.");
+					continue;
+				}
 
-            String line;
-            String version = "";
-            String link = "";
-            try {
-                version = fix.version_fix;
-                link = fix.urlFix;
-            } catch (Exception ignored) {
-            }
+				try (JarFile pluginJar = new JarFile(pom)) {
+					try (BufferedReader reader = new BufferedReader(new InputStreamReader(pluginJar.getInputStream(pluginJar.getJarEntry("plugin.yml"))))) {
+						String line;
+						while ((line = reader.readLine()) != null)
+							allLines += line + "\n";
+					}
+				}
+			}
 
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.startsWith("version:") && !line.equals("version: " + version)) {
-                    versionFound = true;
-                }
+			for (Fix fix : fixes) {
+				if(!entries.contains(fix.main.replaceAll("\\.", "/") + ".class")) continue;
 
-                if (line.startsWith("main:") && line.equals("main: " + mainClassSplit.replaceAll("/", "\\.").replace(".class", "")))
-                    mainFound = true;
-            }
-            bufferedReader.close();
-            jarFile.close();
+				if(plugins.equals(PLUGIN)) {
+					if(fix.type.equals(FIX)) {
+						if(!allLines.contains("version: " + fix.version) && allLines.contains("main: " + fix.main)) {
+							System.out.println(i18n.get("update.pluginversion", pom.getName(), fix.version, fix.repo, fix.aim));
+							System.out.println(i18n.get("update.downloadpluginversion", pom.getName()));
+							if(new Scanner(System.in).next().equals("yes"))
+								downloadFile(fix.url, pom);
+						}
+					} else if(fix.type.equals(NOT_COMPATIBLE))
+						if(allLines.contains("main: " + fix.main))
+							delete("plugins", pom);
 
-            if (!version.equals("")) {
-                if (versionFound && mainFound) {
-                    System.out.println(i18n.get("update.pluginversion", new Object[]{file.getName().replace(".jar", ""), version, link, ""}));
-                    System.out.println(i18n.get("update.downloadpluginversion", new Object[]{file.getName().replace(".jar", "")}));
-                    if (new Scanner(System.in).next().equals("yes"))
-                        downloadFile(link, new File(plugins + "/" + file.getName()));
-                }
-            }
-        }
-    }
+				} else delete("mods", pom);
+			}
+		}
+	}
 
-    public static void chekPlugins(String mainClass) throws Exception {
-        String mainClassSplit = mainClass.replaceAll("\\.", "/") + ".class";
+	private static void delete(String name, File f) throws Exception {
+		System.out.println(i18n.get("update.deleting", f.getName(), name));
+		File newPath = new File("delete/" + name + "/" + f.getName());
+		newPath.getParentFile().mkdirs();
+		Files.copy(f.toPath(), newPath.toPath(), REPLACE_EXISTING);
+		f.delete();
+	}
 
-        File plugins = new File("plugins");
-        if (!plugins.exists()) plugins.mkdir();
-        for (File f : plugins.listFiles((dir, name) -> name.endsWith(".jar"))) {
-            if (fileExists(f, "plugin.yml") && fileExists(f, mainClassSplit)) {
-                System.out.println(i18n.get("update.deleting", new Object[]{f.getName(), "plugins"}));
-                System.gc();
-                Thread.sleep(100);
-                File newf = new File("delete/plugins");
-                File qnewf = new File("delete", f.getPath());
-                if (!newf.exists()) {
-                    newf.mkdirs();
-                } else {
-                    if (qnewf.exists()) qnewf.delete();
-                }
-                Files.copy(f.toPath(), qnewf.toPath(), REPLACE_EXISTING);
-                f.delete();
-            }
-        }
-    }
+	private static ArrayList<String> jarEntries(File f) {
+		try (JarFile jf = new JarFile(f)) {
+			return jf.stream().sequential().map(ZipEntry::getName).collect(Collectors.toCollection(ArrayList<String>::new));
+		} catch (Exception e) {
+			System.out.println("[WARN] - The jar file " + f.getName() + " (at " + f.getAbsolutePath() + ") is maybe corrupted or empty.");
+			return null;
+		}
+	}
 
-    public static void chekMods(Fix fix) {
+	public static class Fix {
 
-    }
+		public String main;
+		public String url;
+		public String version;
+		public String repo;
+		public String type;
+		public String aim;
 
-    public static void chekMods(String mainClass) throws Exception {
-        String mainClassSplit = mainClass.replaceAll("\\.", "/") + ".class";
+		public Fix(String mainClass, String url, String version, String repo, String aim) {
+			this.main = mainClass;
+			this.url = url;
+			this.version = version;
+			this.repo = repo;
+			this.type = FIX;
+			this.aim = aim;
+		}
 
-        File mods = new File("mods");
-        if (!mods.exists()) mods.mkdir();
-        for (File f : mods.listFiles((dir, name) -> name.endsWith(".jar"))) {
-            if (fileExists(f, mainClassSplit)) {
-                System.out.println(i18n.get("update.deleting", new Object[]{f.getName(), "mods"}));
-                System.gc();
-                Thread.sleep(100);
-                File newf = new File("delete/mods");
-                File qnewf = new File("delete", f.getPath());
-                if (!newf.exists()) {
-                    newf.mkdirs();
-                } else {
-                    if (qnewf.exists()) qnewf.delete();
-                }
-                Files.copy(f.toPath(), qnewf.toPath(), REPLACE_EXISTING);
-                f.delete();
-            }
-        }
-    }
-
-    private static boolean fileExists(File file, String fileSearch) {
-        if (!file.exists()) return false;
-        try {
-            JarFile jarFile = new JarFile(file);
-            if (jarFile.getJarEntry(fileSearch) != null) {
-                jarFile.close();
-                return true;
-            }
-        } catch (Exception e) {
-            System.out.println("[Mohist | ALERT] - The jar file " + file.getName() + " (at " + file.getAbsolutePath() + ") is maybe corrupted or empty.");
-        }
-        return false;
-    }
-
-    public static class Fix {
-
-        public String mainClass;
-        public String urlFix;
-        public String version_fix;
-
-        public Fix(String mainClass, String url_fix, String version_fix) {
-            this.mainClass = mainClass;
-            this.urlFix = url_fix;
-            this.version_fix = version_fix;
-        }
-    }
+		public Fix(String mainClass) {
+			this.main = mainClass;
+			this.type = NOT_COMPATIBLE;
+		}
+	}
 }
