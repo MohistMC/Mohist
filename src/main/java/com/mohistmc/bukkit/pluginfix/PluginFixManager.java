ab367d151eb81a4deb9cf36e296ece04c3bba95f
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-06-22 11:13:14 +0200

    fix worldedit directly in Mohist

--- src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
@@ -3,7 +3,12 @@ package com.mohistmc.bukkit.pluginfix;
 public class PluginFixManager {
 
     public static byte[] injectPluginFix(String className, byte[] clazz) {
-
+        if (className.equals("com.sk89q.worldedit.bukkit.WorldEditPlugin")) {
+            return WorldEditFix.replaceGetKeyByGetKeyForge(clazz);
+        }
+        if(className.equals("com.sk89q.worldedit.bukkit.BukkitAdapter")) {
+            return WorldEditFix.replaceAdaptBlockType(clazz);
+        }
         return clazz;
     }
 
---
+++ src/main/java/com/mohistmc/bukkit/pluginfix/WorldEditFix.java
@@ -0,0 +1,70 @@
+package com.mohistmc.bukkit.pluginfix;
+
+import org.bukkit.Material;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.*;
+
+import java.util.ListIterator;
+import java.util.Locale;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.objectweb.asm.Opcodes.*;
+
+public class WorldEditFix {
+
+    public static byte[] replaceGetKeyByGetKeyForge(byte[] basicClass) {
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassNode classNode = new ClassNode();
+        ClassWriter classWriter = new ClassWriter(0);
+        classReader.accept(classNode, 0);
+
+        for (MethodNode methodNode : classNode.methods) {
+            ListIterator<AbstractInsnNode> nodeListIterator = methodNode.instructions.iterator();
+            while (nodeListIterator.hasNext()) {
+                AbstractInsnNode nextNode = nodeListIterator.next();
+                if (nextNode instanceof MethodInsnNode) {
+                    MethodInsnNode methodInsnNode = (MethodInsnNode) nextNode;
+                    if (methodInsnNode.owner.equals("org/bukkit/Material") && methodInsnNode.name.equals("getKey") && methodInsnNode.desc.equals("()Lorg/bukkit/NamespacedKey;")) {
+                        methodInsnNode.name = "getKeyForge";
+                    }
+                }
+            }
+        }
+
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+    
+    public static byte[] replaceAdaptBlockType(byte[] basicClass) {
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassNode classNode = new ClassNode();
+        ClassWriter classWriter = new ClassWriter(0);
+        classReader.accept(classNode, 0);
+
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals("adapt") && methodNode.desc.equals("(Lcom/sk89q/worldedit/world/block/BlockType;)Lorg/bukkit/Material;")) {
+                InsnList toInject = new InsnList();
+                toInject.add(new VarInsnNode(ALOAD, 0));
+                toInject.add(new MethodInsnNode(INVOKEVIRTUAL, "com/sk89q/worldedit/world/block/BlockType", "getId", "()Ljava/lang/String;"));
+                toInject.add(new MethodInsnNode(INVOKESTATIC, Type.getInternalName(WorldEditFix.class), "adaptFix", "(Ljava/lang/String;)Lorg/bukkit/Material;"));
+                toInject.add(new InsnNode(ARETURN));
+                methodNode.instructions = toInject;
+            }
+        }
+
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+
+    // Method inject in worldedit plugin
+    public static Material adaptFix(String blockType) {
+        checkNotNull(blockType);
+        if (!blockType.startsWith("minecraft:")) {
+            return Material.getMaterial(blockType.replace(":", "_").toUpperCase(Locale.ROOT));
+        }
+        return Material.getMaterial(blockType.substring(10).toUpperCase(Locale.ROOT));
+    }
+}
