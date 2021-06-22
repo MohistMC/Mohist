95bb96d207fa513e7474c447674f0b1ab16b9fa1
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-06-22 11:14:24 +0200

    remove worledit in plugin fix

--- src/fmllauncher/java/com/mohistmc/util/AutoDeletePlugins.java
+++ src/fmllauncher/java/com/mohistmc/util/AutoDeletePlugins.java
@@ -2,6 +2,7 @@ package com.mohistmc.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author Mgazul
@@ -9,11 +10,5 @@ import java.util.Arrays;
  */
 public class AutoDeletePlugins {
 
-    public static ArrayList<PluginsModsDelete.Fix> LIST = new ArrayList<>(Arrays.asList(
-
-			new PluginsModsDelete.Fix("com.sk89q.worldedit.bukkit.WorldEditPlugin",
-					"https://github.com/MohistMC/WorldEdit/releases/download/1.16.5-1.0/worldedit-bukkit-7.3.0-MOHIST-dist.jar",
-					"\"7.3.0-MOHIST+cf3b83f\"",
-					"https://github.com/MohistMC/WorldEdit/tree/1.16.5",
-					"This fix make modded blocks compatible")));
+    public static ArrayList<PluginsModsDelete.Fix> LIST = new ArrayList<>(Collections.emptyList());
 }
