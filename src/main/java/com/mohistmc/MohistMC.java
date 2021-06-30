6095f3daa36437408131b2b179b32720479be9ce
Mgazul <mgazul@163.com>
2021-06-30 14:09:07 +0800

    Revert "Fix special characters for Windows"
    This reverts commit 270ad107e93e18ca5ddb9125fd5075e172886ad7.

--- src/main/java/com/mohistmc/MohistMC.java
+++ src/main/java/com/mohistmc/MohistMC.java
@@ -10,63 +10,54 @@ import com.mohistmc.network.download.DownloadJava;
 import com.mohistmc.network.download.UpdateUtils;
 import com.mohistmc.util.EulaUtil;
 import com.mohistmc.util.i18n.Message;
-import org.apache.logging.log4j.Logger;
-
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import org.apache.logging.log4j.Logger;
 
 public class MohistMC {
-	public static final String NAME = "Mohist";
-	public static Logger LOGGER;
-	public static ArrayList<String> mainArgs = null;
+    public static final String NAME = "Mohist";
+    public static Logger LOGGER;
+    public static ArrayList<String> mainArgs = null;
 
-	public static String getVersion() {
-		return (MohistMC.class.getPackage().getImplementationVersion() != null) ? MohistMC.class.getPackage().getImplementationVersion() : "unknown";
-	}
+    public static String getVersion() {
+        return (MohistMC.class.getPackage().getImplementationVersion() != null) ? MohistMC.class.getPackage().getImplementationVersion() : "unknown";
+    }
 
-	public static void main(String[] args) throws Throwable {
-		mainArgs = new ArrayList<>(Arrays.asList(args));
-		MohistConfigUtil.copyMohistConfig();
-		if(Float.parseFloat(System.getProperty("java.class.version")) != 52.0 || MohistConfigUtil.bMohist("use_custom_java8", "false"))
-			DownloadJava.run();
-		if(MohistConfigUtil.bMohist("showlogo")) {
-			System.out.println("\n" + "\n" +
-					" __    __   ______   __  __   __   ______   ______  \n" +
-					"/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
-					"\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
-					" \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
-					"  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
-					"                                                    \n" + "\n");
-			System.out.println("                                      " + Message.getString("forge.serverlanunchwrapper.1"));
-		}
-		if(System.getProperty("log4j.configurationFile") == null)
-			System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
+    public static void main(String[] args) throws Throwable {
+        mainArgs = new ArrayList<>(Arrays.asList(args));
+        MohistConfigUtil.copyMohistConfig();
+        if (Float.parseFloat(System.getProperty("java.class.version")) != 52.0 || MohistConfigUtil.bMohist("use_custom_java8", "false"))
+            DownloadJava.run();
+        if (MohistConfigUtil.bMohist("showlogo")) {
+            System.out.println("\n" + "\n" +
+                    " __    __   ______   __  __   __   ______   ______  \n" +
+                    "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
+                    "\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
+                    " \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
+                    "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
+                    "                                                    \n" + "\n");
+            System.out.println("                                      " + Message.getString("forge.serverlanunchwrapper.1"));
+        }
+        if (System.getProperty("log4j.configurationFile") == null) {
+            System.setProperty("log4j.configurationFile", "log4j2_mohist.xml");
+        }
 
-		if(DownloadJava.os().equals("Windows")) {
-			System.setProperty("file.encoding", "cp850");
-			Field charset = Charset.class.getDeclaredField("defaultCharset");
-			charset.setAccessible(true);
-			charset.set(null, null);
-		}
+        if (MohistConfigUtil.bMohist("check_libraries")) DefaultLibraries.run();
+        DefaultLibraries.loadDefaultLibs();
+        CustomLibraries.loadCustomLibs();
 
-		if(MohistConfigUtil.bMohist("check_libraries")) DefaultLibraries.run();
-		DefaultLibraries.loadDefaultLibs();
-		CustomLibraries.loadCustomLibs();
+        if (MohistConfigUtil.bMohist("check_update")) UpdateUtils.versionCheck();
+        Class.forName("com.google.gson.internal.bind.TypeAdapters$EnumTypeAdapter").getClassLoader();
+        if (!EulaUtil.hasAcceptedEULA()) {
+            System.out.println(Message.getString("eula"));
+            while (!"true".equals(new Scanner(System.in).next()));
+            EulaUtil.writeInfos();
+        }
 
-		if(MohistConfigUtil.bMohist("check_update")) UpdateUtils.versionCheck();
-		Class.forName("com.google.gson.internal.bind.TypeAdapters$EnumTypeAdapter").getClassLoader();
-		if(!EulaUtil.hasAcceptedEULA()) {
-			System.out.println(Message.getString("eula"));
-			while (!"true".equals(new Scanner(System.in).next())) ;
-			EulaUtil.writeInfos();
-		}
-
-		if(!MohistConfigUtil.bMohist("disable_plugins_blacklist", "false")) AutoDeletePlugins.jar();
-		if(!MohistConfigUtil.bMohist("disable_mods_blacklist", "false")) AutoDeleteMods.jar();
-		FastWorkBenchConf.changeConf();
-		Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper").getDeclaredMethod("main", String[].class).invoke(null, new Object[]{args});
-	}
+        if (!MohistConfigUtil.bMohist("disable_plugins_blacklist", "false")) AutoDeletePlugins.jar();
+        if (!MohistConfigUtil.bMohist("disable_mods_blacklist", "false")) AutoDeleteMods.jar();
+        FastWorkBenchConf.changeConf();
+        Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper").getDeclaredMethod("main", String[].class).invoke(null, new Object[]{args});
+    }
 }
