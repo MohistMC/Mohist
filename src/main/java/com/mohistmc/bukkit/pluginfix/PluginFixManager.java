514a95fb2236c52911d6298f7387c3d7f80f6473
KR33PY <kr33pyrmc@gmail.com>
2021-07-01 01:56:54 +0300

    Add 'enable_worldedit_fix' option to control WE fix application

--- src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
@@ -2,6 +2,7 @@ package com.mohistmc.bukkit.pluginfix;
 
 import com.mohistmc.bukkit.pluginfix.fix.ActivePaperOptimization;
 import com.mohistmc.bukkit.pluginfix.fix.WorldEditFix;
+import com.mohistmc.config.MohistConfigUtil;
 
 public class PluginFixManager {
 
@@ -9,10 +10,10 @@ public class PluginFixManager {
         if (ActivePaperOptimization.plugin_work_with_paper_optimization.contains(className)) {
             clazz = ActivePaperOptimization.activeIfItWorkWithPaperOptimization(clazz);
         }
-        if (className.equals("com.sk89q.worldedit.bukkit.WorldEditPlugin")) {
+        if (className.equals("com.sk89q.worldedit.bukkit.WorldEditPlugin") && MohistConfigUtil.bMohist("enable_worldedit_fix", "true")) {
             return WorldEditFix.replaceGetKeyByGetKeyForge(clazz);
         }
-        if(className.equals("com.sk89q.worldedit.bukkit.BukkitAdapter")) {
+        if(className.equals("com.sk89q.worldedit.bukkit.BukkitAdapter") && MohistConfigUtil.bMohist("enable_worldedit_fix", "true")) {
             return WorldEditFix.replaceAdaptBlockType(clazz);
         }
         if (className.endsWith("PaperLib")) {
