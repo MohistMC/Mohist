1f11c477d4496785a1b5a5d3eb815b4e4fcb473a
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-07-04 19:50:14 +0200

    add paper server patch: 0418 0418-Load-Chunks-for-Login-Asynchronously

--- src/main/java/com/destroystokyo/paper/config/PaperWorldConfig.java
+++ src/main/java/com/destroystokyo/paper/config/PaperWorldConfig.java
@@ -9,69 +9,85 @@ import static com.destroystokyo.paper.config.PaperConfig.log;
 
 public class PaperWorldConfig {
 
-	private final String worldName;
-	private final SpigotWorldConfig spigotConfig;
-	private YamlConfiguration config;
-	private boolean verbose;
+    private final String worldName;
+    private final SpigotWorldConfig spigotConfig;
+    private YamlConfiguration config;
+    private boolean verbose;
 
-	public PaperWorldConfig(String worldName, SpigotWorldConfig spigotConfig) {
-		this.worldName = worldName;
-		this.spigotConfig = spigotConfig;
-		this.config = PaperConfig.config;
-		init();
-	}
+    public PaperWorldConfig(String worldName, SpigotWorldConfig spigotConfig) {
+        this.worldName = worldName;
+        this.spigotConfig = spigotConfig;
+        this.config = PaperConfig.config;
+        init();
+    }
 
-	public void init() {
-		this.config = PaperConfig.config; // grab updated reference
-		log("-------- World Settings For [" + worldName + "] --------");
-		PaperConfig.readConfig(PaperWorldConfig.class, this);
-	}
+    public void init() {
+        this.config = PaperConfig.config; // grab updated reference
+        log("-------- World Settings For [" + worldName + "] --------");
+        PaperConfig.readConfig(PaperWorldConfig.class, this);
+    }
 
-	private void set(String path, Object val) {
-		config.set("world-settings.default." + path, val);
-		if (config.get("world-settings." + worldName + "." + path) != null) {
-			config.set("world-settings." + worldName + "." + path, val);
-		}
-	}
+    private void set(String path, Object val) {
+        config.set("world-settings.default." + path, val);
+        if (config.get("world-settings." + worldName + "." + path) != null) {
+            config.set("world-settings." + worldName + "." + path, val);
+        }
+    }
 
-	public boolean perPlayerMobSpawns = false;
-	private void perPlayerMobSpawns() {
-		perPlayerMobSpawns = getBoolean("per-player-mob-spawns", false);
-	}
+    public boolean perPlayerMobSpawns = false;
 
-	private boolean getBoolean(String path, boolean def) {
-		config.addDefault("world-settings.default." + path, def);
-		return config.getBoolean("world-settings." + worldName + "." + path, config.getBoolean("world-settings.default." + path));
-	}
+    private void perPlayerMobSpawns() {
+        perPlayerMobSpawns = getBoolean("per-player-mob-spawns", false);
+    }
 
-	private double getDouble(String path, double def) {
-		config.addDefault("world-settings.default." + path, def);
-		return config.getDouble("world-settings." + worldName + "." + path, config.getDouble("world-settings.default." + path));
-	}
+    private boolean getBoolean(String path, boolean def) {
+        config.addDefault("world-settings.default." + path, def);
+        return config.getBoolean("world-settings." + worldName + "." + path, config.getBoolean("world-settings.default." + path));
+    }
 
-	private int getInt(String path, int def) {
-		config.addDefault("world-settings.default." + path, def);
-		return config.getInt("world-settings." + worldName + "." + path, config.getInt("world-settings.default." + path));
-	}
+    private double getDouble(String path, double def) {
+        config.addDefault("world-settings.default." + path, def);
+        return config.getDouble("world-settings." + worldName + "." + path, config.getDouble("world-settings.default." + path));
+    }
 
-	private float getFloat(String path, float def) {
-		// TODO: Figure out why getFloat() always returns the default value.
-		return (float) getDouble(path, def);
-	}
+    private int getInt(String path, int def) {
+        config.addDefault("world-settings.default." + path, def);
+        return config.getInt("world-settings." + worldName + "." + path, config.getInt("world-settings.default." + path));
+    }
 
-	private <T> List<T> getList(String path, List<T> def) {
-		config.addDefault("world-settings.default." + path, def);
-		return (List<T>) config.getList("world-settings." + worldName + "." + path, config.getList("world-settings.default." + path));
-	}
+    private float getFloat(String path, float def) {
+        // TODO: Figure out why getFloat() always returns the default value.
+        return (float) getDouble(path, def);
+    }
 
-	private String getString(String path, String def) {
-		config.addDefault("world-settings.default." + path, def);
-		return config.getString("world-settings." + worldName + "." + path, config.getString("world-settings.default." + path));
-	}
+    private <T> List<T> getList(String path, List<T> def) {
+        config.addDefault("world-settings.default." + path, def);
+        return (List<T>) config.getList("world-settings." + worldName + "." + path, config.getList("world-settings.default." + path));
+    }
 
-	public short keepLoadedRange;
-	private void keepLoadedRange() {
-		keepLoadedRange = (short) (getInt("keep-spawn-loaded-range", Math.min(spigotConfig.viewDistance, 10)) * 16);
-		log("Keep Spawn Loaded Range: " + (keepLoadedRange / 16));
-	}
+    private String getString(String path, String def) {
+        config.addDefault("world-settings.default." + path, def);
+        return config.getString("world-settings." + worldName + "." + path, config.getString("world-settings.default." + path));
+    }
+
+    public short keepLoadedRange;
+    private void keepLoadedRange() {
+        keepLoadedRange = (short) (getInt("keep-spawn-loaded-range", Math.min(spigotConfig.viewDistance, 10)) * 16);
+        log("Keep Spawn Loaded Range: " + (keepLoadedRange / 16));
+    }
+
+    public int autoSavePeriod = -1;
+    private void autoSavePeriod() {
+        autoSavePeriod = getInt("auto-save-interval", -1);
+        if (autoSavePeriod > 0) {
+            log("Auto Save Interval: " + autoSavePeriod + " (" + (autoSavePeriod / 20) + "s)");
+        } else if (autoSavePeriod < 0) {
+            autoSavePeriod = net.minecraft.server.MinecraftServer.getServer().autosavePeriod;
+        }
+    }
+
+    public int maxAutoSaveChunksPerTick = 24;
+    private void maxAutoSaveChunksPerTick() {
+        maxAutoSaveChunksPerTick = getInt("max-auto-save-chunks-per-tick", 24);
+    }
 }
