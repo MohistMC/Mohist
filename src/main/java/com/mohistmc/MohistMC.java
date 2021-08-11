aa6936d28decd5ff2a4dd6bac8acd6febc77720a
Mgazul <mgazul@163.com>
2021-08-11 08:30:13 +0800

    Update MohistMC.java

--- src/main/java/com/mohistmc/MohistMC.java
+++ src/main/java/com/mohistmc/MohistMC.java
@@ -5,5 +5,5 @@ import org.apache.logging.log4j.Logger;
 
 public class MohistMC {
     public static final String NAME = "Mohist";
-    public static Logger LOGGER =  LogManager.getLogger();
+    public static Logger LOGGER = LogManager.getLogger();
 }
