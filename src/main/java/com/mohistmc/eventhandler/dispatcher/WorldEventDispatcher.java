d133cf433598a2029adaf0a5d700b7d034bf0ddd
1798643961 <1798643961@qq.com>
2021-09-14 20:01:24 +0800

    Handle WorldEvent

---
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/WorldEventDispatcher.java
@@ -0,0 +1,33 @@
+package com.mohistmc.eventhandler.dispatcher;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
+import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.WorldSaveEvent;
+
+public class WorldEventDispatcher {
+
+    //For WorldLoadEvent
+    @SubscribeEvent(receiveCanceled = true)
+    public void onWorldLoadEvent(WorldEvent.Load event) {
+        if (Bukkit.getServer() instanceof CraftServer) {
+            ServerWorld handle = ((CraftWorld) event.getWorld()).getHandle();
+            WorldLoadEvent load = new WorldLoadEvent(handle.getWorld());
+            Bukkit.getPluginManager().callEvent(load);
+        }
+    }
+
+    //For WorldSaveEvent
+    @SubscribeEvent(receiveCanceled = true)
+    public void onWorldSaveEvent(WorldEvent.Save event) {
+        if (Bukkit.getServer() instanceof CraftServer) {
+            ServerWorld handle = ((CraftWorld) event.getWorld()).getHandle();
+            WorldSaveEvent save = new WorldSaveEvent(handle.getWorld());
+            Bukkit.getPluginManager().callEvent(save);
+        }
+    }
+}
