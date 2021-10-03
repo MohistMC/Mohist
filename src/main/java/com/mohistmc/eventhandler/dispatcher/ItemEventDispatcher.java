9a0842fe0cdebd42d0bbecd66e27538381daefb9
1798643961 <1798643961@qq.com>
2021-10-03 16:04:30 +0800

    Handler ItemDespawnEvent

---
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/ItemEventDispatcher.java
@@ -0,0 +1,24 @@
+package com.mohistmc.eventhandler.dispatcher;
+
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraftforge.event.entity.item.ItemExpireEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_16_R3.event.CraftEventFactory;
+
+public class ItemEventDispatcher {
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void onItemExpireEvent(ItemExpireEvent event) {
+        if (Bukkit.getServer() instanceof CraftServer) {
+            // CraftBukkit start - fire ItemDespawnEvent
+            ItemEntity entity = event.getEntityItem();
+            if (CraftEventFactory.callItemDespawnEvent(((ItemEntity) (Object) this)).isCancelled()) {
+                entity.age = 0;
+                return;
+            }
+            // CraftBukkit end
+        }
+    }
+}
