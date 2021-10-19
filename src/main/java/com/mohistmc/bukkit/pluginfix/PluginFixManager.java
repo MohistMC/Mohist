1df321adc8ee5711bebdde610d77fbe984eb1fc3
Mgazul <mgazul@163.com>
2021-10-19 19:33:52 +0800

    Don't let the plugin think we are paper

--- src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
@@ -1,6 +1,5 @@
 package com.mohistmc.bukkit.pluginfix;
 
-import com.mohistmc.bukkit.pluginfix.fix.ActivePaperOptimization;
 import com.mohistmc.bukkit.pluginfix.fix.MythicMobFix;
 import com.mohistmc.bukkit.pluginfix.fix.WorldEditFix;
 import com.mohistmc.configuration.MohistConfig;
@@ -8,9 +7,6 @@ import com.mohistmc.configuration.MohistConfig;
 public class PluginFixManager {
 
     public static byte[] injectPluginFix(String className, byte[] clazz) {
-        if (ActivePaperOptimization.plugin_work_with_paper_optimization.contains(className)) {
-            clazz = ActivePaperOptimization.activeIfItWorkWithPaperOptimization(clazz);
-        }
         if (className.equals("com.sk89q.worldedit.bukkit.WorldEditPlugin") && MohistConfig.instance.enableworldeditfix.getValue()) {
             return WorldEditFix.replaceGetKeyByGetKeyForgeAndGetAsString(clazz);
         }
@@ -19,9 +15,6 @@ public class PluginFixManager {
         }
         if (className.equals("io.lumine.xikage.mythicmobs.volatilecode.v1_16_R3.VolatileWorldHandler_v1_16_R3")) {
             return MythicMobFix.patchVolatileWorldHandler(clazz);
-        }
-        if (className.endsWith("PaperLib")) {
-            return ActivePaperOptimization.activePaperOptimization(clazz);
         }
         return clazz;
     }
