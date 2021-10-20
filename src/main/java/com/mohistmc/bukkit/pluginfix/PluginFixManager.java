8de9c8c05c4df318a8e83b7608edbd1aeb945475
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-10-20 16:55:57 +0200

    Don't detect paper in mohist for plugin

--- src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
@@ -3,6 +3,13 @@ package com.mohistmc.bukkit.pluginfix;
 import com.mohistmc.bukkit.pluginfix.fix.MythicMobFix;
 import com.mohistmc.bukkit.pluginfix.fix.WorldEditFix;
 import com.mohistmc.configuration.MohistConfig;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.*;
+
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+import static org.objectweb.asm.Opcodes.IRETURN;
 
 public class PluginFixManager {
 
@@ -16,7 +23,31 @@ public class PluginFixManager {
         if (className.equals("io.lumine.xikage.mythicmobs.volatilecode.v1_16_R3.VolatileWorldHandler_v1_16_R3")) {
             return MythicMobFix.patchVolatileWorldHandler(clazz);
         }
+        if (className.endsWith("PaperLib")) {
+            return PluginFixManager.removePaper(clazz);
+        }
         return clazz;
+    }
+
+    public static byte[] removePaper(byte[] basicClass) {
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassNode classNode = new ClassNode();
+        ClassWriter classWriter = new ClassWriter(0);
+        classReader.accept(classNode, 0);
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals("isPaper") && methodNode.desc.equals("()Z")) {
+                InsnList toInject = new InsnList();
+                toInject.add(new MethodInsnNode(INVOKESTATIC, Type.getInternalName(PluginFixManager.class), "isPaper", "()Z"));
+                toInject.add(new InsnNode(IRETURN));
+                methodNode.instructions = toInject;
+            }
+        }
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+
+    public static boolean isPaper() {
+        return false;
     }
 
 }
