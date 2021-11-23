5978ad7ead92dba51a421c7652fd655f69a046e8
InkerBot <2924657226@qq.com>
2021-11-23 23:43:11 +0800

    Path.of() incorrectly in Github action

--- buildSrc/src/main/groovy/com/mohistmc/tasks/Env.groovy
+++ buildSrc/src/main/groovy/com/mohistmc/tasks/Env.groovy
@@ -3,7 +3,6 @@ package com.mohistmc.tasks
 import groovy.json.JsonSlurper
 
 import java.nio.file.Files
-import java.nio.file.Path
 
 class Env {
     private static final data
@@ -12,10 +11,12 @@ class Env {
         def envFile = new File("env.json");
         def defaultFile = new File("env.default.json");
         if (!envFile.exists()) {
+            def generateFile = new File("buildSrc/src/main/resources/env.generate.json")
             Files.copy(
-                    Path.of("buildSrc/src/main/resources/env.generate.json"),
-                    Path.of("env.json")
+                    generateFile.toPath(),
+                    envFile.toPath()
             )
+            // Don't use Path.of(), It works incorrectly in Github actions
         }
         def jsonSluper = new JsonSlurper()
         data = jsonSluper.parse(new FileReader(envFile))
