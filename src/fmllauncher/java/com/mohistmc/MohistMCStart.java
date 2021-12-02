52fc75cf7a65281216be2736977f083fa103d7d4
shawiizz <shaweereso@gmail.com>
2021-12-02 20:57:29 +0100

    Add -noserver launch argument to prevent Minecraft server launch.

--- src/fmllauncher/java/com/mohistmc/MohistMCStart.java
+++ src/fmllauncher/java/com/mohistmc/MohistMCStart.java
@@ -14,47 +14,50 @@ import static com.mohistmc.util.EulaUtil.hasAcceptedEULA;
 import static com.mohistmc.util.EulaUtil.writeInfos;
 import static com.mohistmc.util.InstallUtils.startInstallation;
 import static com.mohistmc.util.PluginsModsDelete.checkPlugins;
+import static net.minecraftforge.server.ServerMain.mainArgs;
 
 public class MohistMCStart {
 
-    public static String getVersion() {
-        return (MohistMCStart.class.getPackage().getImplementationVersion() != null) ? MohistMCStart.class.getPackage().getImplementationVersion() : "unknown";
-    }
+	public static String getVersion() {
+		return (MohistMCStart.class.getPackage().getImplementationVersion() != null) ? MohistMCStart.class.getPackage().getImplementationVersion() : "unknown";
+	}
 
-    public static void main() throws Exception {
-        MohistConfigUtil.copyMohistConfig();
-        CustomFlagsHandler.handleCustomArgs();
+	public static void main() throws Exception {
+		MohistConfigUtil.copyMohistConfig();
+		CustomFlagsHandler.handleCustomArgs();
 
-        if (MohistConfigUtil.bMohist("show_logo", "true"))
-            System.out.println("\n" + "\n" +
-                    " __    __   ______   __  __   __   ______   ______  \n" +
-                    "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
-                    "\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
-                    " \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
-                    "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
-                    "                                                    \n" + "\n" +
-                    "                                      " + i18n.get("mohist.launch.welcomemessage"));
-        if (MohistConfigUtil.bMohist("check_libraries", "true")) {
-            DefaultLibraries.run();
-            startInstallation();
-        }
-        CustomLibraries.loadCustomLibs();
-        new JarLoader().loadJar(InstallUtils.extra);
+		if(MohistConfigUtil.bMohist("show_logo", "true"))
+			System.out.println("\n" + "\n" +
+					" __    __   ______   __  __   __   ______   ______  \n" +
+					"/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
+					"\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
+					" \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
+					"  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
+					"                                                    \n" + "\n" +
+					"                                      " + i18n.get("mohist.launch.welcomemessage"));
+		if(MohistConfigUtil.bMohist("check_libraries", "true")) {
+			DefaultLibraries.run();
+			startInstallation();
+		}
+		CustomLibraries.loadCustomLibs();
+		new JarLoader().loadJar(InstallUtils.extra);
 
-        // make sure gson use this EnumTypeAdapter
-        Class.forName("com.google.gson.internal.bind.TypeAdapters$EnumTypeAdapter").getClassLoader();
-        System.setOut(new LoggingPrintStream("STDOUT", System.out, Level.INFO));
-        System.setErr(new LoggingPrintStream("STDERR", System.err, Level.ERROR));
-        if (MohistConfigUtil.bMohist("check_update", "true")) UpdateUtils.versionCheck();
-        if (!hasAcceptedEULA()) {
-            System.out.println(i18n.get("eula"));
-            while (!"true".equals(new Scanner(System.in).next())) ;
-            writeInfos();
-        }
-        if (!MohistConfigUtil.bMohist("disable_plugins_blacklist", "false"))
+		// make sure gson use this EnumTypeAdapter
+		Class.forName("com.google.gson.internal.bind.TypeAdapters$EnumTypeAdapter").getClassLoader();
+		System.setOut(new LoggingPrintStream("STDOUT", System.out, Level.INFO));
+		System.setErr(new LoggingPrintStream("STDERR", System.err, Level.ERROR));
+		if(MohistConfigUtil.bMohist("check_update", "true")) UpdateUtils.versionCheck();
+		if(mainArgs.contains("-noserver"))
+			System.exit(0); //-noserver -> Do not run the Minecraft server, only let the installation running.
+		if(!hasAcceptedEULA()) {
+			System.out.println(i18n.get("eula"));
+			while (!"true".equals(new Scanner(System.in).next())) ;
+			writeInfos();
+		}
+		if(!MohistConfigUtil.bMohist("disable_plugins_blacklist", "false"))
 			checkPlugins(AutoDeletePlugins.LIST, PluginsModsDelete.PLUGIN);
 
-        if (!MohistConfigUtil.bMohist("disable_mods_blacklist", "false"))
+		if(!MohistConfigUtil.bMohist("disable_mods_blacklist", "false"))
 			checkPlugins(AutoDeleteMods.LIST, PluginsModsDelete.MOD);
-    }
+	}
 }
