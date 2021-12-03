79118cd8d6848f4e84022591b7b340a40ab56310
Guarda Panda <joao.pedro.espanhol@hotmail.com>
2021-12-03 22:09:29 +0000

    Bug Reappearance Fix

--- src/main/java/com/mohistmc/eventhandler/dispatcher/PlayerEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/PlayerEventDispatcher.java
@@ -2,11 +2,9 @@ package com.mohistmc.eventhandler.dispatcher;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerEventDispatcher {
 
@@ -16,15 +14,6 @@ public class PlayerEventDispatcher {
         if (event.getPlayer() instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
             Bukkit.getPluginManager().callEvent(new PlayerAdvancementDoneEvent(player.getBukkitEntity(), event.getAdvancement().bukkit));
-        }
-    }
-
-    //For PlayerRespawnEvent
-    @SubscribeEvent
-    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        if (event.getPlayer() instanceof ServerPlayerEntity) {
-            ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
-            Bukkit.getPluginManager().callEvent(new PlayerRespawnEvent(player.getBukkitEntity(), player.getBukkitEntity().getLocation(), false, false));
         }
     }
 }
