6a0636c9f0f5ff0bdeb7f17416b48843890a54e2
Mgazul <mgazul@163.com>
2021-12-03 17:44:18 +0800

    Temporarily handle player respawn events

--- src/main/java/com/mohistmc/eventhandler/dispatcher/PlayerEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/PlayerEventDispatcher.java
@@ -2,9 +2,11 @@ package com.mohistmc.eventhandler.dispatcher;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerEventDispatcher {
 
@@ -14,6 +16,15 @@ public class PlayerEventDispatcher {
         if (event.getPlayer() instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
             Bukkit.getPluginManager().callEvent(new PlayerAdvancementDoneEvent(player.getBukkitEntity(), event.getAdvancement().bukkit));
+        }
+    }
+
+    //For PlayerRespawnEvent
+    @SubscribeEvent
+    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
+        if (event.getPlayer() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+            Bukkit.getPluginManager().callEvent(new PlayerRespawnEvent(player.getBukkitEntity(), player.getBukkitEntity().getLocation(), false, false));
         }
     }
 }
