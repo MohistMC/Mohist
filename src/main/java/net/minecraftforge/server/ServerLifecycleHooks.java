439890ae97dfb7228eb60933bce6cd2d5862d3b0
Mgazul <mgazul@163.com>
2022-01-03 20:16:37 +0800

    Update Forge to 39.0.10

--- src/main/java/net/minecraftforge/server/ServerLifecycleHooks.java
+++ src/main/java/net/minecraftforge/server/ServerLifecycleHooks.java
@@ -111,7 +111,6 @@ public class ServerLifecycleHooks
     public static void handleServerStopping(final MinecraftServer server)
     {
         allowLogins.set(false);
-        PermissionAPI.initializePermissionAPI();
         MinecraftForge.EVENT_BUS.post(new ServerStoppingEvent(server));
     }
 
