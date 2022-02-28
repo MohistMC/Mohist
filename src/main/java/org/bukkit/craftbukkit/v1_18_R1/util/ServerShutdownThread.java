c7b0ca01ece26a109b47b2b421a2793013abcafb
Malcolm <malcolm.kohne03@gmail.com>
2022-02-28 19:52:15 +0100

    some patches
    only a few patches left

--- src/main/java/org/bukkit/craftbukkit/v1_18_R1/util/ServerShutdownThread.java
+++ src/main/java/org/bukkit/craftbukkit/v1_18_R1/util/ServerShutdownThread.java
@@ -1,6 +1,7 @@
 package org.bukkit.craftbukkit.v1_18_R1.util;
 
 import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.LogManager;
 
 public class ServerShutdownThread extends Thread {
     private final MinecraftServer server;
@@ -11,6 +12,12 @@ public class ServerShutdownThread extends Thread {
 
     @Override
     public void run() {
+
+        // FORGE: Halting as GameTestServer will cause issues as it always calls System#exit on both crash and normal exit, so skip it
+        if (!(server instanceof net.minecraft.gametest.framework.GameTestServer))
+            server.halt(true);
+        LogManager.shutdown(); // we're manually managing the logging shutdown on the server. Make sure we do it here at the end.
+
         try {
             server.close();
         } finally {
