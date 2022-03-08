4735769dff71e3f8424bff6675ba35e70353cf16
Alexey <alexey.sirotin.2015@gmail.com>
2022-03-08 17:54:51 +0600

    Fixed an issue where the CraftChunkSnapshot chunk snapshot was not getting the correct mod block id.

--- src/main/java/org/bukkit/craftbukkit/v1_12_R1/CraftChunk.java
+++ src/main/java/org/bukkit/craftbukkit/v1_12_R1/CraftChunk.java
@@ -249,13 +249,17 @@ public class CraftChunk implements Chunk {
 
                 byte[] rawIds = new byte[4096];
                 NibbleArray data = new NibbleArray();
-                cs[i].getData().getDataForNBT(rawIds, data);
+                NibbleArray blockIdExtensionData = cs[i].getData().getDataForNBT(rawIds, data); // mirrol
 
                 byte[] dataValues = sectionBlockData[i] = data.getData();
 
                 // Copy base IDs
                 for (int j = 0; j < 4096; j++) {
-                    blockids[j] = (short) (rawIds[j] & 0xFF);
+                    // mirrol start
+                    int extData = blockIdExtensionData == null ? 0 : blockIdExtensionData.get(j & 15, j >> 8 & 15, j >> 4 & 15);
+                    int blockId = (extData << 12 | (rawIds[j] & 0xFF) << 4 | extData) >> 4;
+                    blockids[j] = blockId > Short.MAX_VALUE ? 0 : (short)blockId; // Not support id > 32767
+                    // mirrol end
                 }
 
                 sectionBlockIDs[i] = blockids;
