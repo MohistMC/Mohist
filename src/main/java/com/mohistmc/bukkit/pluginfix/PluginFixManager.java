83bd436c13799882715ea9b1ad85826a2d822c87
Mgazul <mgazul@163.com>
2022-10-30 22:00:54 +0800

    Prevent Mohist from being recognized as paper (FAWE)

--- src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/bukkit/pluginfix/PluginFixManager.java
@@ -2,18 +2,50 @@ package com.mohistmc.bukkit.pluginfix;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+
+
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
+import static org.objectweb.asm.Opcodes.IRETURN;
 
 public class PluginFixManager {
 
     public static byte[] injectPluginFix(String className, byte[] clazz) {
+        if (className.endsWith("PaperLib")) {
+            return PluginFixManager.removePaper(clazz);
+        }
         if (className.equals("com.earth2me.essentials.utils.VersionUtil")) {
             return helloWorld(clazz, "net.minecraftforge.common.MinecraftForge", "hello.World");
         }
         return clazz;
+    }
+
+    public static byte[] removePaper(byte[] basicClass) {
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassNode classNode = new ClassNode();
+        ClassWriter classWriter = new ClassWriter(0);
+        classReader.accept(classNode, 0);
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals("isPaper") && methodNode.desc.equals("()Z")) {
+                InsnList toInject = new InsnList();
+                toInject.add(new MethodInsnNode(INVOKESTATIC, Type.getInternalName(PluginFixManager.class), "isPaper", "()Z"));
+                toInject.add(new InsnNode(IRETURN));
+                methodNode.instructions = toInject;
+            }
+        }
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+
+    public static boolean isPaper() {
+        return false;
     }
 
     public static byte[] helloWorld(byte[] basicClass, String a, String b) {
---
+++ src/main/java/com/mohistmc/bukkit/pluginfix/fix/DynmapFix.java
@@ -0,0 +1,55 @@
+/*
+ * Mohist - MohistMC
+ * Copyright (C) 2018-2022.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.mohistmc.bukkit.pluginfix.fix;
+
+import com.mohistmc.bukkit.nms.utils.RemapUtils;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+
+import static org.objectweb.asm.Opcodes.ARETURN;
+
+public class DynmapFix {
+
+    public static byte[] replaceBukkitVersionHelperSpigot116_4(byte[] basicClass) {
+        ClassReader classReader = new ClassReader(basicClass);
+        ClassNode classNode = new ClassNode();
+        ClassWriter classWriter = new ClassWriter(0);
+        classReader.accept(classNode, 0);
+
+        for (MethodNode method : classNode.methods) {
+            if (method.name.equals("getNMSPackage")) {
+                InsnList insnList = new InsnList();
+                insnList.add(new LdcInsnNode(RemapUtils.nmspackage));
+                insnList.add(new InsnNode(ARETURN));
+                method.instructions = insnList;
+                method.tryCatchBlocks.clear(); // fix java.lang.ClassFormatError: Illegal exception table range in class file
+            }
+        }
+
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+}
