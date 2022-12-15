4117dd8912a744d925283dd5f688e844fda9d30b
lliioollcn <lliiooll@126.com>
2022-12-15 20:16:41 +0800

    add mohist to modlist

--- src/main/java/com/mohistmc/MohistMC.java
+++ src/main/java/com/mohistmc/MohistMC.java
@@ -1,9 +1,19 @@
 package com.mohistmc;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Mod("mohist")
+@OnlyIn(Dist.DEDICATED_SERVER)
 public class MohistMC {
     public static final String NAME = "Mohist";
     public static Logger LOGGER = LogManager.getLogger();
+
+    public MohistMC() {
+        //TODO: do something when mod loading
+        LOGGER.info("Mohist加载中");
+    }
 }
