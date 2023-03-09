a76f48841ff95c43bb11962462a8077420140e36
1798643961 <1798643961@qq.com>
2023-03-09 22:28:55 +0800

    to:fixed event canceled

--- src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
@@ -145,7 +145,8 @@ public class BlockEventDispatcher {
             org.bukkit.event.player.PlayerInteractEvent bukkitEvent = CraftEventFactory.callPlayerInteractEvent(serverPlayer,
                     Action.LEFT_CLICK_BLOCK, event.getPos(), event.getFace(),
                     serverPlayer.getInventory().getSelected(), InteractionHand.MAIN_HAND);
-            if (bukkitEvent.isCancelled()) {
+            if (bukkitEvent.isCancelled()
+                    || event.isCanceled()) {
                 // Let the client know the block still exists
                 serverPlayer.connection.send(new ClientboundBlockUpdatePacket(event.getLevel().getMinecraftWorld(), event.getPos()));
                 // Update any tile entity data for this block
@@ -154,6 +155,7 @@ public class BlockEventDispatcher {
                     serverPlayer.connection.send(tileentity.getUpdatePacket());
                 }
                 event.setCancellationResult(InteractionResult.FAIL);
+                event.setCanceled(true);
             }
             // CraftBukkit end
         }
