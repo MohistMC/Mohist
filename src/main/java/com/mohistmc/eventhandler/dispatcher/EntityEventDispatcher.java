7bdc6debae9dfaea151adfd3ef4f8893c41dea67
Mgazul <mgazul@163.com>
2023-04-25 02:10:27 +0800

    Fix wrong entity type
    https://haste.mohistmc.com/okuyowuqak.yaml

--- src/main/java/com/mohistmc/eventhandler/dispatcher/EntityEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/EntityEventDispatcher.java
@@ -138,11 +138,12 @@ public class EntityEventDispatcher {
 
     @SubscribeEvent(receiveCanceled = true)
     public void changeTargetEvent(EntityTeleportEvent event) {
-        ServerPlayer serverplayer = (ServerPlayer)event.getEntity();
-        CraftPlayer player = serverplayer.getBukkitEntity();
-        org.bukkit.Location location = player.getLocation();
-        PlayerTeleportEvent teleEvent = new PlayerTeleportEvent(player, player.getLocation(), location, PlayerTeleportEvent.TeleportCause.ENDER_PEARL);
-        Bukkit.getPluginManager().callEvent(teleEvent);
-        event.setCanceled(teleEvent.isCancelled());
+        if (event.getEntity() instanceof ServerPlayer serverplayer) {
+            CraftPlayer player = serverplayer.getBukkitEntity();
+            org.bukkit.Location location = player.getLocation();
+            PlayerTeleportEvent teleEvent = new PlayerTeleportEvent(player, player.getLocation(), location, PlayerTeleportEvent.TeleportCause.ENDER_PEARL);
+            Bukkit.getPluginManager().callEvent(teleEvent);
+            event.setCanceled(teleEvent.isCancelled());
+        }
     }
 }
