395b7601ef552e5f7ebedfecc0210014b248548c
Mgazul <mgazul@163.com>
2023-06-16 20:55:53 +0800

    Add ResponseCode check

--- src/main/java/com/mohistmc/network/download/DownloadSource.java
+++ src/main/java/com/mohistmc/network/download/DownloadSource.java
@@ -4,6 +4,11 @@ package com.mohistmc.network.download;
 import com.mohistmc.configuration.MohistConfigUtil;
 import com.mohistmc.util.i18n.Message;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public enum DownloadSource {
 
     MOHIST("https://maven.mohistmc.com/"),
@@ -17,16 +22,26 @@ public enum DownloadSource {
         this.url = url;
     }
 
-    public static DownloadSource get() {
+    public static DownloadSource get() throws IOException {
         String ds = MohistConfigUtil.sMohist("libraries_downloadsource", defaultSource.name());
         for (DownloadSource me : DownloadSource.values()) {
-            if (me.name().equalsIgnoreCase(ds))
+            if (me.name().equalsIgnoreCase(ds)) {
+                if (isDown(me.url) != 200) return GITHUB;
                 return me;
+            }
         }
         return defaultSource;
     }
 
     public String getUrl() {
         return url;
+    }
+
+    public static int isDown(String s) throws IOException {
+        URL url = new URL(s);
+        URLConnection rulConnection = url.openConnection();
+        HttpURLConnection httpUrlConnection = (HttpURLConnection) rulConnection;
+        httpUrlConnection.connect();
+        return httpUrlConnection.getResponseCode();
     }
 }
