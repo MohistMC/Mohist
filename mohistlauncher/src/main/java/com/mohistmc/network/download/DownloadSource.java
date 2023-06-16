fba6b71b5479dcba552bef4b82dd24a742de2061
Mgazul <mgazul@163.com>
2023-06-16 20:36:06 +0800

    Add ResponseCode check

--- mohistlauncher/src/main/java/com/mohistmc/network/download/DownloadSource.java
+++ mohistlauncher/src/main/java/com/mohistmc/network/download/DownloadSource.java
@@ -24,6 +24,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 @ToString
 @AllArgsConstructor
 public enum DownloadSource {
@@ -37,10 +42,11 @@ public enum DownloadSource {
     final
     String url;
 
-    public static DownloadSource get() {
+    public static DownloadSource get() throws IOException {
         String ds = MohistConfigUtil.defaultSource();
         for (DownloadSource me : DownloadSource.values()) {
             if (me.name().equalsIgnoreCase(ds)) {
+                if (isDown(me.url) == 520) return GITHUB;
                 return me;
             }
         }
@@ -49,5 +55,13 @@ public enum DownloadSource {
 
     public static boolean isCN() {
         return MohistMCStart.i18n.isCN();
+    }
+
+    public static int isDown(String s) throws IOException {
+        URL url = new URL(s);
+        URLConnection rulConnection = url.openConnection();
+        HttpURLConnection httpUrlConnection = (HttpURLConnection) rulConnection;
+        httpUrlConnection.connect();
+        return httpUrlConnection.getResponseCode();
     }
 }
