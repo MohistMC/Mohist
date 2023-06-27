aaa41211a2cebe4d77a14bb81a802f77ccd99e81
Mgazul <mgazul@163.com>
2023-06-27 13:42:56 +0800

    Fix compatibility for mods

--- mohistlauncher/src/main/java/com/mohistmc/network/download/DownloadSource.java
+++ mohistlauncher/src/main/java/com/mohistmc/network/download/DownloadSource.java
@@ -24,6 +24,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+@Getter
 @ToString
 @AllArgsConstructor
 public enum DownloadSource {
@@ -33,17 +39,25 @@ public enum DownloadSource {
     GITHUB("https://mavenmirror.mohistmc.com/");
 
     public static final DownloadSource defaultSource = i18n.isCN() ? CHINA : MOHIST;
-    @Getter
     final
     String url;
 
-    public static DownloadSource get() {
+    public static DownloadSource get() throws IOException {
         String ds = MohistConfigUtil.defaultSource();
         for (DownloadSource me : DownloadSource.values()) {
             if (me.name().equalsIgnoreCase(ds)) {
+                if (isDown(me.url) != 200) return GITHUB;
                 return me;
             }
         }
         return defaultSource;
+    }
+
+    public static int isDown(String s) throws IOException {
+        URL url = new URL(s);
+        URLConnection rulConnection = url.openConnection();
+        HttpURLConnection httpUrlConnection = (HttpURLConnection) rulConnection;
+        httpUrlConnection.connect();
+        return httpUrlConnection.getResponseCode();
     }
 }
