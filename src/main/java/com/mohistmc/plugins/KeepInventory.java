73b0d03e3b90e7409232ffdf3f7fd04fa477b045
Mgazul <mgazul@163.com>
2023-07-24 14:40:53 +0800

    Optimize respawn logic

--- src/main/java/com/mohistmc/plugins/KeepInventory.java
+++ src/main/java/com/mohistmc/plugins/KeepInventory.java
@@ -1,7 +1,8 @@
 package com.mohistmc.plugins;
 
 import com.mohistmc.MohistConfig;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.GameRule;
 
 /**
  * @author Mgazul by MohistMC
@@ -9,21 +10,17 @@ import net.minecraft.world.level.Level;
  */
 public class KeepInventory {
 
-    public static boolean inventory(Level serverLevel) {
-        String world = serverLevel.getWorld().getName();
-        if (MohistConfig.keepinventory_global) {
-            return MohistConfig.keepinventory_inventory;
-        } else {
-            return MohistConfig.yml.getBoolean("keepinventory." + world + ".inventory");
-        }
+    public static boolean inventory(ServerPlayer player) {
+        String world = player.getBukkitEntity().getWorld().getName();
+        boolean i = MohistConfig.keepinventory_global ? MohistConfig.keepinventory_inventory : MohistConfig.yml.getBoolean("keepinventory." + world + ".inventory");
+        player.getBukkitEntity().getWorld().setGameRule(GameRule.KEEP_INVENTORY, i);
+        return i;
     }
 
-    public static boolean exp(Level serverLevel) {
-        String world = serverLevel.getWorld().getName();
-        if (MohistConfig.keepinventory_global) {
-            return MohistConfig.keepinventory_exp;
-        } else {
-            return MohistConfig.yml.getBoolean("keepinventory." + world + ".exp");
-        }
+    public static boolean exp(ServerPlayer player) {
+        String world = player.getBukkitEntity().getWorld().getName();
+        boolean i = MohistConfig.keepinventory_global ? MohistConfig.keepinventory_exp : MohistConfig.yml.getBoolean("keepinventory." + world + ".exp");
+        player.keepLevel = i;
+        return i;
     }
 }
