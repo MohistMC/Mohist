93583f561ae39e1187e7f5094a4363d051291ce4
Mgazul <mgazul@163.com>
2023-07-25 20:06:27 +0800

    Fixed BlockPlaceEvent don't trigger

--- src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
@@ -1,10 +1,13 @@
 package com.mohistmc.eventhandler.dispatcher;
 
+import com.mohistmc.block.MohistBlockSnapshot;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
@@ -17,12 +20,19 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_20_R1.block.CraftBlock;
 import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockMultiPlaceEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockEventDispatcher {
 
@@ -109,6 +119,61 @@ public class BlockEventDispatcher {
                 }
             }
             // CraftBukkit end
+        }
+    }
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void onBlockPlace(BlockEvent.EntityPlaceEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof ServerPlayer serverPlayer) {
+            org.bukkit.entity.Player player = serverPlayer.getBukkitEntity();
+            Direction direction = event.getPlaceEventDirection();
+            if (direction != null) {
+                InteractionHand hand = event.getPlaceEventHand();
+                CraftBlock placedBlock = MohistBlockSnapshot.fromBlockSnapshot(event.getBlockSnapshot(), true);
+                CraftBlock againstBlock = CraftBlock.at(event.getLevel(), event.getPos().relative(direction.getOpposite()));
+                org.bukkit.inventory.ItemStack bukkitStack;
+                org.bukkit.inventory.EquipmentSlot bukkitHand;
+                if (hand == InteractionHand.MAIN_HAND) {
+                    bukkitStack = player.getInventory().getItemInMainHand();
+                    bukkitHand = org.bukkit.inventory.EquipmentSlot.HAND;
+                } else {
+                    bukkitStack = player.getInventory().getItemInOffHand();
+                    bukkitHand = org.bukkit.inventory.EquipmentSlot.OFF_HAND;
+                }
+                BlockPlaceEvent placeEvent = new BlockPlaceEvent(placedBlock, placedBlock.getState(), againstBlock, bukkitStack, player, !event.isCanceled(), bukkitHand);
+                placeEvent.setCancelled(event.isCanceled());
+                Bukkit.getPluginManager().callEvent(placeEvent);
+                event.setCanceled(placeEvent.isCancelled() || !placeEvent.canBuild());
+            }
+        }
+    }
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void onMultiPlace(BlockEvent.EntityMultiPlaceEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof ServerPlayer serverPlayer) {
+            org.bukkit.entity.Player player = serverPlayer.getBukkitEntity();
+            Direction direction = event.getPlaceEventDirection();
+            if (direction != null) {
+                InteractionHand hand = event.getPlaceEventHand();
+                List<org.bukkit.block.BlockState> placedBlocks = new ArrayList<>(event.getReplacedBlockSnapshots().size());
+                for (BlockSnapshot snapshot : event.getReplacedBlockSnapshots()) {
+                    placedBlocks.add(MohistBlockSnapshot.fromBlockSnapshot(snapshot, true).getState());
+                }
+                CraftBlock againstBlock = CraftBlock.at(event.getLevel(), event.getPos().relative(direction.getOpposite()));
+                org.bukkit.inventory.ItemStack bukkitStack;
+                if (hand == InteractionHand.MAIN_HAND) {
+                    bukkitStack = player.getInventory().getItemInMainHand();
+                } else {
+                    bukkitStack = player.getInventory().getItemInOffHand();
+                }
+                BlockPlaceEvent placeEvent = new BlockMultiPlaceEvent(placedBlocks, againstBlock, bukkitStack, player, !event.isCanceled()
+                );
+                placeEvent.setCancelled(event.isCanceled());
+                Bukkit.getPluginManager().callEvent(placeEvent);
+                event.setCanceled(placeEvent.isCancelled() || !placeEvent.canBuild());
+            }
         }
     }
 }
