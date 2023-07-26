a7b9149315e22f751c7c7b908158b39e3f2025c0
Mgazul <mgazul@163.com>
2023-07-26 22:53:24 +0800

    Fix KeepInventory

--- src/main/java/com/mohistmc/plugins/KeepInventory.java
+++ src/main/java/com/mohistmc/plugins/KeepInventory.java
@@ -2,6 +2,7 @@ package com.mohistmc.plugins;
 
 import com.mohistmc.MohistConfig;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.GameRules;
 import org.bukkit.GameRule;
 
 /**
@@ -11,6 +12,9 @@ import org.bukkit.GameRule;
 public class KeepInventory {
 
     public static boolean inventory(ServerPlayer player) {
+        if (player.level().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+            return true;
+        }
         String world = player.getBukkitEntity().getWorld().getName();
         boolean i = MohistConfig.keepinventory_global ? MohistConfig.keepinventory_inventory : MohistConfig.yml.getBoolean("keepinventory." + world + ".inventory");
         player.getBukkitEntity().getWorld().setGameRule(GameRule.KEEP_INVENTORY, i);
@@ -18,6 +22,9 @@ public class KeepInventory {
     }
 
     public static boolean exp(ServerPlayer player) {
+        if (player.level().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+            return true;
+        }
         String world = player.getBukkitEntity().getWorld().getName();
         boolean i = MohistConfig.keepinventory_global ? MohistConfig.keepinventory_exp : MohistConfig.yml.getBoolean("keepinventory." + world + ".exp");
         player.keepLevel = i;
