d3b8203de7f9ee9f1b4e6496ccf00e8be290a406
Mgazul <mgazul@163.com>
2023-08-17 01:44:47 +0800

    Fix some bug

--- src/main/java/net/minecraftforge/common/ForgeConfig.java
+++ src/main/java/net/minecraftforge/common/ForgeConfig.java
@@ -39,13 +39,13 @@ public class ForgeConfig {
                     .comment("Set this to true to remove any BlockEntity that throws an error in its update method instead of closing the server and reporting a crash log. BE WARNED THIS COULD SCREW UP EVERYTHING USE SPARINGLY WE ARE NOT RESPONSIBLE FOR DAMAGES.")
                     .translation("forge.configgui.removeErroringBlockEntities")
                     .worldRestart()
-                    .define("removeErroringBlockEntities", false);
+                    .define("removeErroringBlockEntities", true);
 
             removeErroringEntities = builder
                     .comment("Set this to true to remove any Entity (Note: Does not include BlockEntities) that throws an error in its tick method instead of closing the server and reporting a crash log. BE WARNED THIS COULD SCREW UP EVERYTHING USE SPARINGLY WE ARE NOT RESPONSIBLE FOR DAMAGES.")
                     .translation("forge.configgui.removeErroringEntities")
                     .worldRestart()
-                    .define("removeErroringEntities", false);
+                    .define("removeErroringEntities", true);
 
             fullBoundingBoxLadders = builder
                     .comment("Set this to true to check the entire entity's collision bounding box for ladders instead of just the block they are in. Causes noticeable differences in mechanics so default is vanilla behavior. Default: false.")
