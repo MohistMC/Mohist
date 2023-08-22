9cd1a0ca9bee1469c5a92b2f3086344e019c7742
Mgazul <mgazul@163.com>
2023-08-23 00:34:13 +0800

    Some change

--- forge/fmlcore/src/main/java/net/minecraftforge/fml/ModWorkManager.java
+++ forge/fmlcore/src/main/java/net/minecraftforge/fml/ModWorkManager.java
@@ -22,12 +22,15 @@ public class ModWorkManager {
         boolean driveOne();
 
         default void drive(Runnable ticker) {
+            boolean ranOne = false;
             if (!selfDriven()) {
                 ticker.run();
                 while (true) {
                     if (!driveOne()) break;
+                    ranOne = true;
                 }
-            } else {
+            }
+            if(!ranOne) {
                 // park for a bit so other threads can schedule
                 LockSupport.parkNanos(PARK_TIME);
             }
