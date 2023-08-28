d8974cead8e708d4f477e860d02d45e75efd6443
Mgazul <mgazul@163.com>
2023-08-28 19:50:27 +0800

    [TODO] Uses a weird way to be compatible with Bungeecord

--- src/main/java/net/minecraftforge/server/ServerLifecycleHooks.java
+++ src/main/java/net/minecraftforge/server/ServerLifecycleHooks.java
@@ -69,6 +69,7 @@ import net.minecraftforge.forgespi.language.IModInfo;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.GameData;
 import org.jetbrains.annotations.ApiStatus;
+import org.spigotmc.SpigotConfig;
 
 public class ServerLifecycleHooks
 {
@@ -171,7 +172,7 @@ public class ServerLifecycleHooks
                 return false;
             }
 
-            if (connectionType == ConnectionType.VANILLA && !NetworkRegistry.acceptsVanillaClientConnections()) {
+            if (!SpigotConfig.bungee && connectionType == ConnectionType.VANILLA && !NetworkRegistry.acceptsVanillaClientConnections()) {
                 rejectConnection(manager, connectionType, "This server has mods that require Forge to be installed on the client. Contact your server admin for more details.");
                 return false;
             }
