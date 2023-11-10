fb63a35dc524e4bff2860e6694205f04e6e8eb01
Mgazul <mgazul@163.com>
2023-11-10 22:24:35 +0800

    Auto ignore ConnectionType when using velocity or bungee

--- src/main/java/com/mohistmc/util/ProxyUtils.java
+++ src/main/java/com/mohistmc/util/ProxyUtils.java
@@ -10,6 +10,6 @@ import org.spigotmc.SpigotConfig;
 public class ProxyUtils {
 
     public static boolean ignoreRejected() {
-        return MohistConfig.ignoreConnectionType && (MohistConfig.velocity_enabled || SpigotConfig.bungee);
+        return /*MohistConfig.ignoreConnectionType && */ (MohistConfig.velocity_enabled || SpigotConfig.bungee);
     }
 }
