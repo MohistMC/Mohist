3738bf16b2a084a5ef9f508c73b056fccd6e6163
Mgazul <mgazul@163.com>
2023-11-22 14:46:53 +0800

    The mod's SpawnCategory does not perform spawn-limits restrictions

--- src/main/java/org/bukkit/craftbukkit/v1_19_R1/util/CraftSpawnCategory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_19_R1/util/CraftSpawnCategory.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.SpawnCategory;
 public class CraftSpawnCategory {
 
     public static boolean isValidForLimits(SpawnCategory spawnCategory) {
-        return spawnCategory != null && spawnCategory != SpawnCategory.MISC;
+        return spawnCategory != null && spawnCategory.ordinal() < SpawnCategory.MISC.ordinal();
     }
 
     public static String getConfigNameSpawnLimit(SpawnCategory spawnCategory) {
@@ -67,7 +67,7 @@ public class CraftSpawnCategory {
             case WATER_AMBIENT -> MobCategory.WATER_AMBIENT;
             case WATER_UNDERGROUND_CREATURE -> MobCategory.UNDERGROUND_WATER_CREATURE;
             case MISC -> MobCategory.MISC;
-            default -> throw new UnsupportedOperationException("Unknown SpawnCategory " + spawnCategory + " for MobCategory");
+            default -> MobCategory.valueOf(spawnCategory.name());
         };
     }
 
