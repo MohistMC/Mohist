4663e083635ba6a623456eb085a668f8e3a13bea
Mgazul <mgazul@163.com>
2024-01-26 21:22:47 +0700

    Fix load worlds

--- src/main/java/org/bukkit/craftbukkit/v1_16_R3/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_16_R3/CraftServer.java
@@ -1003,6 +1003,7 @@ public final class CraftServer implements Server {
         console.initWorld(internal, worlddata, worlddata, worlddata.worldGenSettings());
 
         internal.setSpawnSettings(true, true);
+        getServer().prepareLevels(internal.getChunkSource().chunkMap.progressListener, internal);
 
         getServer().loadSpawn(internal.getChunkSource().chunkMap.progressListener, internal);
 
