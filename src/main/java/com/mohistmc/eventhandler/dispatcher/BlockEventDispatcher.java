b23a0708cb5aa9a6632d5ba7de271017565b14b7
Mgazul <mgazul@163.com>
2024-02-07 20:24:47 +0700

    Fixed #2837

--- src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
@@ -27,6 +27,8 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_19_R3.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_19_R3.block.CraftBlockState;
+import org.bukkit.craftbukkit.v1_19_R3.block.CraftBlockStates;
 import org.bukkit.craftbukkit.v1_19_R3.event.CraftEventFactory;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -145,7 +147,9 @@ public class BlockEventDispatcher {
                     bukkitStack = player.getInventory().getItemInOffHand();
                     bukkitHand = org.bukkit.inventory.EquipmentSlot.OFF_HAND;
                 }
-                BlockPlaceEvent placeEvent = new BlockPlaceEvent(placedBlock, placedBlock.getState(), againstBlock, bukkitStack, player, !event.isCanceled(), bukkitHand);
+                CraftBlockState replacedBlockState = CraftBlockStates.getBlockState(event.getLevel(), event.getPos());
+                replacedBlockState.setData(event.getBlockSnapshot().getReplacedBlock());
+                BlockPlaceEvent placeEvent = new BlockPlaceEvent(placedBlock, replacedBlockState, againstBlock, bukkitStack, player, !event.isCanceled(), bukkitHand);
                 placeEvent.setCancelled(event.isCanceled());
                 Bukkit.getPluginManager().callEvent(placeEvent);
                 event.setCanceled(placeEvent.isCancelled() || !placeEvent.canBuild());
