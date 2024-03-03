7d8c7056f0aa3f00c6238f39ca4380a1782f4dd5
Mgazul <mgazul@163.com>
2024-03-03 22:05:06 +0800

    Fixed #2985

--- src/main/java/com/mohistmc/plugins/world/utils/WorldsGUI.java
+++ src/main/java/com/mohistmc/plugins/world/utils/WorldsGUI.java
@@ -2,6 +2,7 @@ package com.mohistmc.plugins.world.utils;
 
 import com.mohistmc.api.ItemAPI;
 import com.mohistmc.plugins.MessageI18N;
+import com.mohistmc.plugins.world.listener.InventoryClickListener;
 import java.util.ArrayList;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -22,7 +23,7 @@ public class WorldsGUI {
         while (Bukkit.getWorlds().size() > groesse) {
             groesse += 54;
         }
-        WorldListInventory worldListInventory = new WorldListInventory(groesse, name);
+        WorldInventory worldListInventory = new WorldInventory(WorldInventoryType.LIST, groesse, name);
         Inventory inv = worldListInventory.getInventory();
         for (World w : Bukkit.getWorlds()) {
             ArrayList<String> infoLore = new ArrayList<>();
@@ -56,5 +57,6 @@ public class WorldsGUI {
         }
         inv.setItem(53, ItemAPI.doItem(Material.REDSTONE_BLOCK, 1, MessageI18N.WORLDMANAGE_GUI_CLOSE.getKey(), null));
         p.openInventory(inv);
+        InventoryClickListener.worldInventory = worldListInventory;
     }
 }
