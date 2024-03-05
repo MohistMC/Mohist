aa1fd2643581e521dfe2294bbbfbe01dd74b65f6
Mgazul <mgazul@163.com>
2024-03-05 18:07:11 +0800

    Fix Recipe bug

--- src/main/java/org/bukkit/craftbukkit/v1_20_R2/inventory/RecipeIterator.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R2/inventory/RecipeIterator.java
@@ -1,12 +1,15 @@
 package org.bukkit.craftbukkit.v1_20_R2.inventory;
 
 import com.google.common.base.Preconditions;
+import com.mohistmc.bukkit.inventory.MohistSpecialRecipe;
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import java.util.Iterator;
 import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.item.crafting.RecipeType;
+import org.bukkit.Keyed;
+import org.bukkit.craftbukkit.v1_20_R2.util.CraftNamespacedKey;
 import org.bukkit.inventory.Recipe;
 
 public class RecipeIterator implements Iterator<Recipe> {
@@ -39,11 +42,10 @@ public class RecipeIterator implements Iterator<Recipe> {
         }
 
         net.minecraft.world.item.crafting.RecipeHolder<?> recipe = current.next();
-        try {
+        if (recipe.toBukkitRecipe() instanceof Keyed) {
             return recipe.toBukkitRecipe();
-        } catch (Throwable e) {
-            throw new RuntimeException("Error converting recipe " + recipe.id(), e);
         }
+        return new MohistSpecialRecipe(CraftNamespacedKey.fromMinecraft(recipe.id()), recipe.value());
     }
 
     @Override
