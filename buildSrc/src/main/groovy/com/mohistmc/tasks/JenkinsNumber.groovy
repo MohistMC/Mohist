e33decee6960b7e182761ce26e7feb2463c1be7d
Mgazul <mgazul@163.com>
2024-04-18 13:40:59 +0800

    Update JenkinsNumber.groovy

--- buildSrc/src/main/groovy/com/mohistmc/tasks/JenkinsNumber.groovy
+++ buildSrc/src/main/groovy/com/mohistmc/tasks/JenkinsNumber.groovy
@@ -6,7 +6,7 @@ class JenkinsNumber {
 
     static String info() {
         try {
-            def data = new JsonSlurper().parseText(new URL("https://ci.codemc.io/job/MohistMC/job/Mohist-1.20.1/api/json").getText("UTF-8"))
+            def data = new JsonSlurper().parseText(new URL("https://ci.codemc.io/job/MohistMC/job/Mohist-1.20.1-rpg/api/json").getText("UTF-8"))
             return data.builds["number"][0]
         } catch (Exception ignored) {
             return 'dev'
