2044d78872024f482282334c0615638132d7cc56
Mgazul <mgazul@163.com>
2024-05-13 14:43:45 +0800

    Fix some bug

--- src/main/java/com/mohistmc/bukkit/PluginsLibrarySource.java
+++ src/main/java/com/mohistmc/bukkit/PluginsLibrarySource.java
@@ -1,25 +1,18 @@
 package com.mohistmc.bukkit;
 
-import com.mohistmc.tools.ConnectionUtil;
-
 /**
  * @author Mgazul by MohistMC
  * @date 2023/10/8 19:16:34
  */
 public enum PluginsLibrarySource {
 
-    ALIBABA("https://maven.aliyun.com/repository/public/"),
     MAVEN2("https://repo.maven.apache.org/maven2/");
 
     public final String url;
 
-    public static final String DEFAULT = isCN() ? ALIBABA.url : MAVEN2.url;
+    public static final String DEFAULT = MAVEN2.url;
 
     PluginsLibrarySource(String url) {
         this.url = url;
-    }
-
-    public static boolean isCN() {
-        return ConnectionUtil.measureLatency(ALIBABA.url) < ConnectionUtil.measureLatency(MAVEN2.url);
     }
 }
