2532abfb1e6c656465ea4754da7df36af4d0ea07
Mgazul <mgazul@163.com>
2024-05-16 01:28:21 +0800

    Fix mohistJar

--- buildSrc/src/main/groovy/com/mohistmc/tasks/JenkinsNumber.groovy
+++ buildSrc/src/main/groovy/com/mohistmc/tasks/JenkinsNumber.groovy
@@ -6,17 +6,10 @@ class JenkinsNumber {
 
     static String info() {
         try {
-            def conn = new URL("https://ci.codemc.io/job/MohistMC/job/Mohist-1.20.5/api/json").openConnection()
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
-            conn.connect()
-            def root = conn.content.text
-            def jsonSluper = new JsonSlurper()
-
-            String data = jsonSluper.parseText(root)
-            def number = data.substring(data.indexOf("number")).split(",")
-            return Integer.valueOf(number[0].replace("number=", "")).intValue()
+            def data = new JsonSlurper().parseText(new URL("https://ci.codemc.io/job/MohistMC/job/Mohist-1.20.6/api/json").getText("UTF-8"))
+            return data.builds["number"][0]
         } catch (Exception ignored) {
-            return '1.20.4'
+            return 'dev'
         }
     }
 }
