af3d6094ae7754b1ade1b61075856f3d1b9ce742
Mgazul <mgazul@163.com>
2024-06-18 23:33:35 +0800

    Fix some npe

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/generator/structure/CraftStructureType.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/generator/structure/CraftStructureType.java
@@ -1,6 +1,7 @@
 package org.bukkit.craftbukkit.v1_20_R1.generator.structure;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 import org.bukkit.craftbukkit.v1_20_R1.util.CraftNamespacedKey;
@@ -9,11 +10,11 @@ import org.bukkit.generator.structure.StructureType;
 public class CraftStructureType extends StructureType {
 
     public static StructureType minecraftToBukkit(net.minecraft.world.level.levelgen.structure.StructureType<?> minecraft) {
-        if (minecraft == null) {
+        ResourceLocation resourceLocation = BuiltInRegistries.STRUCTURE_TYPE.getKey(minecraft);
+        if (minecraft == null || resourceLocation == null) {
             return null;
         }
-
-        return Registry.STRUCTURE_TYPE.get(CraftNamespacedKey.fromMinecraft(BuiltInRegistries.STRUCTURE_TYPE.getKey(minecraft)));
+        return Registry.STRUCTURE_TYPE.get(CraftNamespacedKey.fromMinecraft(resourceLocation));
     }
 
     public static net.minecraft.world.level.levelgen.structure.StructureType<?> bukkitToMinecraft(StructureType bukkit) {
