b4f17c72b9075602c76223136ce383c6d4c4ee9a
Mgazul <mgazul@163.com>
2024-06-30 05:06:52 +0800

    Fix Block BreakEvent

--- src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
+++ src/main/java/com/mohistmc/eventhandler/dispatcher/BlockEventDispatcher.java
@@ -1,6 +1,7 @@
 package com.mohistmc.eventhandler.dispatcher;
 
 import com.mohistmc.bukkit.block.MohistBlockSnapshot;
+import java.util.concurrent.atomic.AtomicReference;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
@@ -59,70 +60,24 @@ public class BlockEventDispatcher {
         }
     }
 
+    public static AtomicReference<BlockBreakEvent> blockBreakEvent = new AtomicReference<>();
+
     @SubscribeEvent(receiveCanceled = true)
     public void onBlockBreak(BlockEvent.BreakEvent event) {
         LevelAccessor level = event.getLevel();
         BlockPos pos = event.getPos();
         Player player = event.getPlayer();
-        BlockState state = event.getState();
         // CraftBukkit start - fire BlockBreakEvent
         org.bukkit.block.Block bblock = CraftBlock.at(level, pos);
-        BlockBreakEvent bukkitEvent;
-        if (player instanceof final ServerPlayer serverPlayer) {
-            if (level instanceof final ServerLevel serverLevel) {
-                // Sword + Creative mode pre-cancel
-                boolean isSwordNoBreak = !serverPlayer.getMainHandItem().getItem().canAttackBlock(state, serverLevel, pos, serverPlayer);
-
-                // Tell client the block is gone immediately then process events
-                // Don't tell the client if its a creative sword break because its not broken!
-                if (level.getBlockEntity(pos) == null && !isSwordNoBreak) {
-                    ClientboundBlockUpdatePacket packet = new ClientboundBlockUpdatePacket(pos, Blocks.AIR.defaultBlockState());
-                    serverPlayer.connection.send(packet);
-                }
-
-                bukkitEvent = new BlockBreakEvent(bblock, serverPlayer.getBukkitEntity());
-
-                // Sword + Creative mode pre-cancel
-                bukkitEvent.setCancelled(isSwordNoBreak);
-                event.setCanceled(isSwordNoBreak);
-
-                // Calculate default block experience
-                BlockState nmsData = serverLevel.getBlockState(pos);
-                Block nmsBlock = nmsData.getBlock();
-
-                ItemStack itemstack = serverPlayer.getItemBySlot(EquipmentSlot.MAINHAND);
-
-                if (nmsBlock != null
-                        && !bukkitEvent.isCancelled()
-                        && !event.isCanceled()
-                        && !serverPlayer.isCreative()
-                        && serverPlayer.hasCorrectToolForDrops(nmsBlock.defaultBlockState())) {
-                    bukkitEvent.setExpToDrop(event.getExpToDrop());
-                }
-
-                serverLevel.getCraftServer().getPluginManager().callEvent(bukkitEvent);
-
-                if (bukkitEvent.isCancelled()) {
-                    if (isSwordNoBreak) {
-                        bukkitEvent.setCancelled(true);
-                        event.setCanceled(true);
-                    }
-
-                    // Let the client know the block still exists
-                    serverPlayer.connection.send(new ClientboundBlockUpdatePacket(serverLevel, pos));
-
-                    // Brute force all possible updates
-                    for (Direction dir : Direction.values()) {
-                        serverPlayer.connection.send(new ClientboundBlockUpdatePacket(level, pos.relative(dir)));
-                    }
-
-                    // Update any tile entity data for this block
-                    BlockEntity tileentity = serverLevel.getBlockEntity(pos);
-                    if (tileentity != null) {
-                        serverPlayer.connection.send(tileentity.getUpdatePacket());
-                    }
-                    event.setCanceled(true);
-                }
+        if (player instanceof ServerPlayer serverPlayer && !(player instanceof FakePlayer)) {
+            if (level instanceof ServerLevel) {
+                BlockBreakEvent bukkitEvent = new BlockBreakEvent(bblock, serverPlayer.getBukkitEntity());
+                bukkitEvent.setCancelled(event.isCanceled());
+                bukkitEvent.setExpToDrop(event.getExpToDrop());
+                Bukkit.getPluginManager().callEvent(bukkitEvent);
+                blockBreakEvent.set(bukkitEvent);
+                event.setCanceled(bukkitEvent.isCancelled());
+                event.setExpToDrop(bukkitEvent.getExpToDrop());
             }
             // CraftBukkit end
         }
