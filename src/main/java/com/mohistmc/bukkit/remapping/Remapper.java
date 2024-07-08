0297672880a422932854b6ed4647cfe4a6ce1cb4
Mgazul <mgazul@163.com>
2024-07-08 10:35:14 +0800

    Add paper packages remapper

--- src/main/java/com/mohistmc/bukkit/remapping/Remapper.java
+++ src/main/java/com/mohistmc/bukkit/remapping/Remapper.java
@@ -50,6 +50,8 @@ public class Remapper {
         this.toNmsMapping = new JarMapping();
         this.toNmsMapping.packages.put("org/yaml/snakeyaml/", "com/mohistmc/org/yaml/snakeyaml/");
         this.toNmsMapping.packages.put("javax/inject/", "com/mohistmc/javax/inject/");
+        this.toNmsMapping.packages.put("com/destroystokyo/paper/", "com/mohistmc/paper/");
+        this.toNmsMapping.packages.put("io/papermc/paper/", "com/mohistmc/paper/");
         this.toNmsMapping.classes.put("io/netty/util/Version", "com/mohistmc/bukkit/pluginfix/ScriptBlockPlus");
         this.toBukkitMapping = new JarMapping();
         this.inheritanceMap = new InheritanceMap();
