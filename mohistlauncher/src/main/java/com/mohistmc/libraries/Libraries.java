e7ad48ff3efac35ffb0ad893755d1ce8aa7b48ba
Mgazul <mgazul@163.com>
2024-10-02 22:57:16 +0800

    Libraries serialization fixed

--- mohistlauncher/src/main/java/com/mohistmc/libraries/Libraries.java
+++ mohistlauncher/src/main/java/com/mohistmc/libraries/Libraries.java
@@ -30,14 +30,13 @@ public class Libraries {
     String path;
     String md5;
     long size;
-    boolean installer;
 
     public static Libraries from(String line) {
         String[] parts = line.split("\\|");
-        return new Libraries(parts[0], parts[1], Long.parseLong(parts[2]), Boolean.parseBoolean(parts[3]));
+        return new Libraries(parts[0], parts[1], Long.parseLong(parts[2]));
     }
 
     public static Libraries from(File file) {
-        return new Libraries(file.getAbsolutePath(), MD5Util.get(file), file.length(), false);
+        return new Libraries(file.getAbsolutePath(), MD5Util.get(file), file.length());
     }
 }
