043e3549b713e8316d3f81e7a0774748c5cc2d6c
Mgazul <mgazul@163.com>
2024-10-06 22:43:14 +0800

    Delete old files

--- nms-patches/org/bukkit/craftbukkit/entity/CraftEnemy.java
+++
@@ -1,9 +0,0 @@
-package org.bukkit.craftbukkit.entity;
-
-import net.minecraft.world.entity.monster.IMonster;
-import org.bukkit.entity.Enemy;
-
-public interface CraftEnemy extends Enemy {
-
-    IMonster getHandle();
-}
--- nms-patches/org/bukkit/craftbukkit/util/Handleable.java
+++
@@ -1,6 +0,0 @@
-package org.bukkit.craftbukkit.util;
-
-public interface Handleable<M> {
-
-    M getHandle();
-}
