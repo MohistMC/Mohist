2363ad81c50a21e87eaab1dbc39741ba78b74922
deepsleep-v3 <wontfix@hotmail.com>
2025-05-21 18:26:16 +0800

    a interface for i18n of ExceptionHandler.

--- src/main/java/com/mohistmc/features/errhandler/i18n_ErrHandler.java
+++ src/main/java/com/mohistmc/features/errhandler/i18n_ErrHandler.java
@@ -1,7 +1,8 @@
 package com.mohistmc.features.errhandler;
 
+
 public interface i18n_ErrHandler {
   default String J2SE_RAMErr(){
-    return "这些内存对于Mohist还是太小了。\nJ2SE分配到的内存：<J2SEMem>\n目前剩余内存：<RemainderMem>\n目前已用内存：<MemOfUsing>试试在服务器启动参数的后面在附加上-Xmx";
+    return "这些内存对于Mohist还是太小了。\nJ2SE分配到的内存：<J2SEMem>\n目前剩余内存：<RemainderMem>\n目前已用内存：<MemOfUsing>试试在服务器启动参数的后面在附加上-Xmx？";
   }
 }
