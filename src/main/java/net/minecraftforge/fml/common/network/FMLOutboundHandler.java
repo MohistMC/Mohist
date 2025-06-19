bf32db5dadbb7d76b8704b0d7b66d6fb42b3353f
Mgazul <mgazul@163.com>
2025-06-20 02:47:31 +0800

    Fix some mod compatibility

--- src/main/java/net/minecraftforge/fml/common/network/FMLOutboundHandler.java
+++ src/main/java/net/minecraftforge/fml/common/network/FMLOutboundHandler.java
@@ -112,6 +112,9 @@ public class FMLOutboundHandler extends ChannelOutboundHandlerAdapter {
             @Override
             public List<NetworkDispatcher> selectNetworks(Object args, ChannelHandlerContext context, FMLProxyPacket packet)
             {
+                if (packet.getDispatcher() == null) {
+                    return ImmutableList.of();
+                }
                 return ImmutableList.of(packet.getDispatcher());
             }
         },
